b4149fbbf02e301ffaebbc9bb2088004ab5d1d2f
Alan Rostem <arostem@gmail.com>
2021-01-08 17:35:36 +0100

    Forgot to add default value for damage range in gun

--- Gun.cs
+++ Gun.cs
@@ -6,23 +6,28 @@ public class Gun : RayCast2D
 	[Export]
 	public float DamageRange;
 
-    private float _direction = 1;
+	private float _direction = 1;
 	public override void _Ready()
 	{
-        ConfigureScanLine(0);   
+		ConfigureScanLine(0);   
 	}
 
-    public void ScanHit(float angle = 0)
-    {
-        ConfigureScanLine(angle);
-        // TODO: Hit scan logic
-    }
-    
-    private void ConfigureScanLine(float angle)
-    {
-        CastTo = new Vector2(
-                Mathf.Cos(angle) * DamageRange * _direction,
-                Mathf.Sin(angle) * DamageRange
-            );
-    }
+	public void ScanHit(float angle = 0)
+	{
+		ConfigureScanLine(angle);
+		// TODO: Hit scan logic
+	}
+	
+	private void ConfigureScanLine(float angle)
+	{
+		CastTo = new Vector2(
+				Mathf.Cos(angle) * DamageRange * _direction,
+				Mathf.Sin(angle) * DamageRange
+			);
+	}
+
+	public override void _Process(float delta)
+	{
+		ConfigureScanLine(0);
+	}
 }
