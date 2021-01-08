225f39d1e3d612c9512ec8914a9968adba39c051
Alan Rostem <arostem@gmail.com>
2021-01-08 19:33:38 +0100

    Added automatic firing cycle

--- Gun.cs
+++ Gun.cs
@@ -3,13 +3,39 @@ using System;
 
 public class Gun : RayCast2D
 {
-	[Export] public float DamageRange;
+	[Export] public float DamageRange = 50;
+	[Export] public uint ClipSize = 30;
+	[Export] public uint StartingAmmo = 120;
+	[Export] public uint RoundsPerMinute = 600;
+
+	private uint _ammoCount;
+	private bool _isFireButtonPressed = false;
+	private bool _isFiring = false;
 
 	private float _direction = 1;
+	private bool _isEquipped = true;
+	private Timer _fireCycleTimer;
+	private AudioStreamPlayer _firingSoundPlayer;
+
+	public bool IsEquipped
+	{
+		get => _isEquipped;
+		set
+		{
+			Visible = value;
+			_isEquipped = value;
+			if (!value)
+				ResetStats();
+		}
+	}
 
 	public override void _Ready()
 	{
 		ConfigureScanLine(0);
+		_ammoCount = ClipSize;
+		_fireCycleTimer = GetNode<Timer>("FireCycleTimer");
+		_fireCycleTimer.WaitTime = 60f / RoundsPerMinute;
+		_firingSoundPlayer = GetNode<AudioStreamPlayer>("FiringSoundPlayer");
 	}
 
 	public void ScanHit(float angle = 0)
@@ -28,7 +54,14 @@ public class Gun : RayCast2D
 				enemy.QueueFree();
 			}
 		}
+
 		Enabled = false;
+	}
+
+	private void ResetStats()
+	{
+		_isFiring = false;
+		_fireCycleTimer.Stop();
 	}
 
 	private void ConfigureScanLine(float angle)
@@ -41,9 +74,42 @@ public class Gun : RayCast2D
 
 	public override void _Process(float delta)
 	{
+		if (!IsEquipped)
+			return;
+
+
 		if (Input.IsActionPressed("fire"))
 		{
-			ScanHit();
+			_isFireButtonPressed = true;
+			if (!_isFiring)
+			{
+				OnFireCycle();
+				_fireCycleTimer.Start();
+				_isFiring = true;
+			}
 		}
+		else
+		{
+			_isFireButtonPressed = false;
+		}
+	}
+
+	public virtual void OnFire()
+	{
+		ScanHit();
+		_firingSoundPlayer.Play();
+	}
+
+	private void OnFireCycle()
+	{
+		if (_ammoCount == 0 || !_isFireButtonPressed)
+		{
+			_isFiring = false;
+			_fireCycleTimer.Stop();
+			return;
+		}
+
+		OnFire();
+		//_ammoCount--;
 	}
 }
