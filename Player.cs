582a09b10eb3ef24c80409a674db713fca557929
Alan Rostem <arostem@gmail.com>
2021-01-09 13:59:19 +0100

    Added some gun equipping code

--- Player.cs
+++ Player.cs
@@ -1,89 +1,129 @@
 using Godot;
 using System;
+using System.Resources;
 
 public class Player : KinematicBody2D
 {
-	[Export] public uint Score = 500;
+    private static PackedScene _defaultGunScene = GD.Load<PackedScene>("res://Gun.tscn");
+    [Export] public uint Score = 500;
 
-	private static float _speed = 60;
-	private static float _jumpSpeed = 350;
+    private static float _speed = 60;
+    private static float _jumpSpeed = 350;
 
-	private AnimatedSprite _sprite;
-	private Vector2 _vel;
-	private uint _hp = 100;
+    private AnimatedSprite _sprite;
+    private Vector2 _vel;
+    private uint _hp = 100;
 
-	private Timer _regenTickTimer;
-	private Timer _regenStartDelayTimer;
+    private Timer _regenTickTimer;
+    private Timer _regenStartDelayTimer;
 
-	public override void _Ready()
-	{
-		_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
-		_regenTickTimer = GetNode<Timer>("RegenTickTimer");
-		_regenStartDelayTimer = GetNode<Timer>("RegenStartDelayTimer");
-	}
+    private Gun _gun0;
+    private Gun _gun1;
+    public Gun EquippedGun;
 
-	public uint GetHP()
-	{
-		return _hp;
-	}
+    public override void _Ready()
+    {
+        _sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        _regenTickTimer = GetNode<Timer>("RegenTickTimer");
+        _regenStartDelayTimer = GetNode<Timer>("RegenStartDelayTimer");
 
-	public void TakeDamage(uint damage)
-	{
-		if (damage < _hp)
-		{
-			_hp -= damage;
-			_regenTickTimer.Stop();
-			_regenStartDelayTimer.Stop();
-			_regenStartDelayTimer.Start();
-		}
-		else
-			; // TODO: Die
-	}
+        _gun0 = EquippedGun = (Gun) _defaultGunScene.Instance();
+        AddChild(_gun0);
+    }
 
-	public override void _PhysicsProcess(float delta)
-	{
-		_vel.y += Constants.Gravity;
+    public void SwitchGun()
+    {
+        if (EquippedGun == _gun0)
+        {
+            if (_gun1 != null)
+                EquippedGun = _gun1;
+        }
+        else if (EquippedGun == _gun1)
+        {
+            EquippedGun = _gun0;
+        }
+    }
 
-		if (Input.IsActionPressed("left"))
-		{
-			_sprite.Play("run");
-			_sprite.FlipH = true;
-			_vel.x = -_speed;
-		}
-		else if (Input.IsActionPressed("right"))
-		{
-			_sprite.Play("run");
-			_sprite.FlipH = false;
-			_vel.x = _speed;
-		}
-		else
-		{
-			_sprite.Play("idle");
-			_vel.x = 0;
-		}
+    public void PickUpGun(Gun gun)
+    {
+        if (EquippedGun == _gun0)
+        {
+            _gun0.QueueFree();
+            _gun0 = gun;
+        }
+        else if (EquippedGun == _gun1)
+        {
+            _gun1.QueueFree();
+            _gun1 = gun;
+        }
 
-		if (Input.IsActionPressed("jump") && IsOnFloor())
-		{
-			_vel.y = -_jumpSpeed;
-			_sprite.Play("jump");
-		}
+        EquippedGun = gun;
+    }
+    
+    public uint GetHP()
+    {
+        return _hp;
+    }
 
-		_vel = MoveAndSlide(_vel, Constants.Up, true);
-	}
+    public void TakeDamage(uint damage)
+    {
+        if (damage < _hp)
+        {
+            _hp -= damage;
+            _regenTickTimer.Stop();
+            _regenStartDelayTimer.Stop();
+            _regenStartDelayTimer.Start();
+        }
+        else
+        {
+            _hp = 0; // TODO: Die
+        }
+    }
 
-	private void OnCanRegen()
-	{
-		_regenTickTimer.Start();
-	}
+    public override void _PhysicsProcess(float delta)
+    {
+        _vel.y += Constants.Gravity;
 
-	private void OnHeal()
-	{
-		const uint amount = 12;
-		_hp += amount;
-		if (_hp > 100)
-		{
-			_regenTickTimer.Stop();
-			_hp = 100;
-		}
-	}
+        if (Input.IsActionPressed("left"))
+        {
+            _sprite.Play("run");
+            _sprite.FlipH = true;
+            _vel.x = -_speed;
+        }
+        else if (Input.IsActionPressed("right"))
+        {
+            _sprite.Play("run");
+            _sprite.FlipH = false;
+            _vel.x = _speed;
+        }
+        else
+        {
+            _sprite.Play("idle");
+            _vel.x = 0;
+        }
+
+        if (Input.IsActionPressed("jump") && IsOnFloor())
+        {
+            _vel.y = -_jumpSpeed;
+            _sprite.Play("jump");
+        }
+
+        _vel = MoveAndSlide(_vel, Constants.Up, true);
+    }
+
+    private void OnCanRegen()
+    {
+        _regenTickTimer.Start();
+    }
+
+    private void OnHeal()
+    {
+        const uint amount = 12;
+        _hp += amount;
+        if (_hp > 100)
+        {
+            _regenTickTimer.Stop();
+            _hp = 100;
+        }
+    }
 }
