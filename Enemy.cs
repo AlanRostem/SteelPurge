efcb5aa7b9168bff0b7ddd058499cc78b653009b
Alan Rostem <arostem@gmail.com>
2021-01-10 13:18:51 +0100

    Added a spawner

--- Enemy.cs
+++ Enemy.cs
@@ -4,54 +4,64 @@ using System;
 public class Enemy : KinematicBody2D
 {
     private static PackedScene _moneyScene = GD.Load<PackedScene>("res://Money.tscn");
-	private static float WalkSpeed = 40;
-	private float _direction = 1;
-	private Vector2 _vel;
-	private uint _hp = 100;
-	private AnimatedSprite _sprite;
-	private uint _damagePerHit = 34;
-	private Player _playerRef;
-	private Timer _attackTimer;
-	private Area2D _meleeArea;
-    
-	
-	public override void _Ready()
-	{
-		_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
-		_attackTimer = GetNode<Timer>("AttackTimer");
-		_meleeArea = GetNode<Area2D>("Area2D");
-		_sprite.Play("walk");
-	}
+    private static float WalkSpeed = 40;
+    private static float EmergeSpeed = 40;
+    private float _direction = 1;
+    private Vector2 _vel;
+    private uint _hp = 100;
+    private AnimatedSprite _sprite;
+    private uint _damagePerHit = 34;
+    private Player _playerRef;
+    private Timer _attackTimer;
+    private Area2D _meleeArea;
+    public bool Spawning = false;
 
-	public override void _PhysicsProcess(float delta)
-	{
+    public override void _Ready()
+    {
+        _sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        _attackTimer = GetNode<Timer>("AttackTimer");
+        _meleeArea = GetNode<Area2D>("Area2D");
+        _sprite.Play("walk");
+    }
 
-		if (IsOnWall())
-			_direction *= -1;
-		_meleeArea.Position = new Vector2(_direction * 8, 0);
-		_sprite.FlipH = _direction < 0;
-		_vel.x = WalkSpeed * _direction;
-		_vel.y += Constants.Gravity;
-		_vel = MoveAndSlide(_vel, Constants.Up);
-	}
-	
-	private void OnPlayerInMeleeRange(object body)
-	{
-		if (body is Player player)
-		{
-			_playerRef = player;
-			_attackTimer.Start();
-			OnDamagePlayer();
-		}
-	}
-	
-	private void OnPlayerExitMeleeRange(object body)
-	{
-		if (body is Player player)
-		{
-			_attackTimer.Stop();
-		}
-	}
+    public override void _PhysicsProcess(float delta)
+    {
+        if (IsOnWall())
+            _direction *= -1;
+        _meleeArea.Position = new Vector2(_direction * 8, 0);
+        if (!Spawning)
+        {
+            _sprite.FlipH = _direction < 0;
+            _vel.x = WalkSpeed * _direction;
+            _vel.y += Constants.Gravity;
+            _vel = MoveAndSlide(_vel, Constants.Up);
+        }
+        else
+        {
+            var pos = GlobalPosition;
+            pos.y -= EmergeSpeed * delta;
+            GlobalPosition = pos;
+        }
+
+    }
+
+    private void OnPlayerInMeleeRange(object body)
+    {
+        if (body is Player player)
+        {
+            _playerRef = player;
+            _attackTimer.Start();
+            OnDamagePlayer();
+        }
+    }
+
+    private void OnPlayerExitMeleeRange(object body)
+    {
+        if (body is Player player)
+        {
+            _attackTimer.Stop();
+        }
+    }
 
     public void TakeDamage(uint damage)
     {
@@ -67,11 +77,11 @@ public class Enemy : KinematicBody2D
         else
         {
             _hp -= damage;
-		}
+        }
     }
-    
-	private void OnDamagePlayer()
-	{
-		_playerRef.TakeDamage(_damagePerHit);
-	}
+
+    private void OnDamagePlayer()
+    {
+        _playerRef.TakeDamage(_damagePerHit);
+    }
 }
---
+++ Spawner.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public class Spawner : StaticBody2D
+{
+	private static PackedScene _enemyScene = GD.Load<PackedScene>("res://Enemy.tscn");
+	[Export] public uint SpawnCount;
+
+
+	public override void _Ready()
+	{
+	}
+
+
+	private void OnSpawn()
+	{
+		if (SpawnCount-- > 0)
+		{
+			var enemy = (Enemy) _enemyScene.Instance();
+			enemy.GlobalPosition = GlobalPosition;
+			enemy.Spawning = true;
+			GetParent().AddChild(enemy);
+		}
+	}
+
+
+	private void OnEnemyExitSpawner(object body)
+	{
+		if (body is Enemy enemy)
+		{
+			enemy.Spawning = false;
+		}
+	}
+}
