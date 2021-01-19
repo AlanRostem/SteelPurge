7ee83c98ea00ce40af91df044dbbd3335c82f24e
Alan Rostem <arostem@gmail.com>
2021-01-19 22:15:47 +0100

    Fixed up the scenes

--- scenes/entities/Entity.cs
+++ scenes/entities/Entity.cs
@@ -3,19 +3,18 @@ using System;
 
 public class Entity : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
+	private static float Gravity = 600;
+	private static Vector2 Up = new Vector2(0, -1);
+	public Vector2 Velocity;
+	
 	public override void _Ready()
 	{
 		
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	public override void _PhysicsProcess(float delta)
+	{
+		Velocity.y += Gravity * delta;
+		Velocity = MoveAndSlide(Velocity, Up, true);
+	}
 }
