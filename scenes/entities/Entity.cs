97074176adf80e92a2fcc5f01717bb574538423b
Alan Rostem <arostem@gmail.com>
2021-01-19 23:29:53 +0100

    Entities have reference to map

--- scenes/entities/Entity.cs
+++ scenes/entities/Entity.cs
@@ -6,10 +6,20 @@ public class Entity : KinematicBody2D
 	private static float Gravity = 600;
 	private static Vector2 Up = new Vector2(0, -1);
 	public Vector2 Velocity;
-	
+	protected Map ParentMap;
+
 	public override void _Ready()
 	{
-		
+		var parent = GetParent();
+		if (OS.IsDebugBuild())
+		{
+			if (!(parent is Map))
+			{
+				throw new Exception("Parent node is not an instance of Map");
+			}
+		}
+
+		ParentMap = (Map) parent;
 	}
 
 	public override void _PhysicsProcess(float delta)
