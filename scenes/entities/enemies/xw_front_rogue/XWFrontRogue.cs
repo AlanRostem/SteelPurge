e8cf78094a4d3e63909cc29d5179d40ae2f37089
Alan Rostem <arostem@gmail.com>
2021-01-27 21:29:50 +0100

    Creating the first enemy

---
+++ scenes/entities/enemies/xw_front_rogue/XWFrontRogue.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public class XWFrontRogue : Enemy
+{
+	public int Direction = 1;
+}
---
+++ scenes/entities/enemies/xw_front_rogue/XWFrontRogueSprite.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class XWFrontRogueSprite : AnimatedSprite
+{
+    private XWFrontRogue _parent;
+	public override void _Ready()
+    {
+        _parent = GetParent<XWFrontRogue>();
+    }
+
+	public override void _Process(float delta)
+    {
+        FlipH = _parent.Direction < 0;
+
+    }
+}
