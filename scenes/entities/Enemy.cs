0c48be5e9d358c172d0b8479a0c9f29388259fef
Alan Rostem <arostem@gmail.com>
2021-01-29 11:34:00 +0100

    forgot to save scene

--- scenes/entities/Enemy.cs
+++ scenes/entities/Enemy.cs
@@ -4,47 +4,47 @@ using System.Collections;
 
 public class Enemy : Entity
 {
-    private PackedScene _scrapScene = GD.Load<PackedScene>("res://scenes/entities/Scrap.tscn");
+	private PackedScene _scrapScene = GD.Load<PackedScene>("res://scenes/entities/Scrap.tscn");
 
-    [Export] public uint ScrapValue = 50;
+	[Export] public uint ScrapValue = 50;
 
-    [Export] public uint BaseHitPoints = 45;
-    private uint _hp;
+	[Export] public uint BaseHitPoints = 45;
+	private uint _hp;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _hp = BaseHitPoints * ParentMap.Round;
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		_hp = BaseHitPoints * ParentMap.Round;
+	}
 
-    public virtual void OnDie()
-    {
-    }
+	public virtual void OnDie()
+	{
+	}
 
-    public void TakeDamage(uint damage)
-    {
-        if (damage >= _hp)
-        {
-            OnDie();
-            QueueFree();
-            var scrap = (Scrap) _scrapScene.Instance();
-            scrap.Position = Position;
-            scrap.Value = ScrapValue;
-            ParentMap.AddChild(scrap);
-            ParentMap.CurrentExpectedEnemies--;
-            ParentMap.EnemiesOnMap--;
-            _hp = 0;
-        }
-        else
-        {
-            _hp -= damage;
-        }
-    }
+	public void TakeDamage(uint damage)
+	{
+		if (damage >= _hp)
+		{
+			OnDie();
+			QueueFree();
+			var scrap = (Scrap) _scrapScene.Instance();
+			scrap.Position = Position;
+			scrap.Value = ScrapValue;
+			ParentMap.AddChild(scrap);
+			ParentMap.CurrentExpectedEnemies--;
+			ParentMap.EnemiesOnMap--;
+			_hp = 0;
+		}
+		else
+		{
+			_hp -= damage;
+		}
+	}
 
 
-    private void _OnDisappear()
-    {
-        ParentMap.EnemiesOnMap--;
-        QueueFree();
-    }
+	private void _OnDisappear()
+	{
+		ParentMap.EnemiesOnMap--;
+		QueueFree();
+	}
 }
