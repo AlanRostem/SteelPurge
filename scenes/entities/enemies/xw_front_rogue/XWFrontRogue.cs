554d35d4ba48fbee1cdf71eb176670f0c526d559
Alan Rostem <arostem@gmail.com>
2021-02-02 20:57:55 +0100

    Trying to make rogues not go inside each other

--- scenes/entities/enemies/xw_front_rogue/XWFrontRogue.cs
+++ scenes/entities/enemies/xw_front_rogue/XWFrontRogue.cs
@@ -7,15 +7,17 @@ public class XWFrontRogue : Enemy
 	[Export] public uint DamagePerHit = 40;
 	public int Direction = 1;
 	private bool _canSwapDir = true;
-	
+	private XWFrontRogue _otherRogue = null;
+	private bool _pushingOut = false;
+
 	[Signal]
 	public delegate void TriggerDirSwapCooldown();
-	
+
 	private void _OnCanSwap()
 	{
 		_canSwapDir = true;
 	}
-	
+
 	protected override void _OnMovement(float delta)
 	{
 		base._OnMovement(delta);
@@ -26,11 +28,37 @@ public class XWFrontRogue : Enemy
 			_canSwapDir = false;
 			Direction = dir;
 		}
-		Velocity.x = WalkSpeed * Direction;
+
+		if (_pushingOut)
+		{
+			Velocity.x = Position.x - _otherRogue.Position.x;
+		}
+		else
+		{
+			Velocity.x = WalkSpeed * Direction;
+		}
 	}
-	
+
 	private void _OnAttackPlayer()
 	{
 		ParentMap.PlayerRef.TakeDamage(DamagePerHit, Direction);
+	}
+
+	private void _OnRogueCollided(object body)
+	{
+		if (body is XWFrontRogue rogue && rogue != this)
+		{
+            _otherRogue = rogue;
+			_pushingOut = true;
+		}
+	}
+
+
+	private void _OnRogueExited(object body)
+	{
+		if (body is XWFrontRogue rogue && rogue != this)
+		{
+			_pushingOut = false;
+		}
 	}
 }
