0582a2e7f6854d71ed302428972fb0a0524c3e6b
Alan Rostem <arostem@gmail.com>
2021-02-06 15:45:45 +0100

    Added VulnerableHitBox concept

---
+++ scenes/components/VulnerableHitbox.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+/// <summary>
+/// Hitbox component with signals tied to projectiles or hit-scan
+/// entities that can hurt it.
+/// </summary>
+public class VulnerableHitbox : Area2D
+{
+	[Signal]
+	public delegate void Hit(uint damage);
+}
