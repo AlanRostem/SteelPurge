3022b35fe673b8492cf1b9944da14d5fff680936
Alan Rostem <arostem@gmail.com>
2021-02-20 20:33:51 +0100

    Added signals for timer on effects

--- scenes/status_effects/StatusEffect.cs
+++ scenes/status_effects/StatusEffect.cs
@@ -3,14 +3,20 @@ using System;
 
 public class StatusEffect : Node2D
 {
-	[Export] public float Duration = 1;
-	public Entity Subject { get; private set; }
-	
-	public override void _Ready()
-	{
-		Subject = GetParent<Entity>();
-	}
+    [Export] public float Duration = 1;
+    public Entity Subject { get; private set; }
 
-	[Signal]
-	public delegate void End(Entity subject);
+    public override void _Ready()
+    {
+        Subject = GetParent<Entity>();
+    }
+
+    [Signal]
+    public delegate void End(Entity subject);
+
+    private void OnEnd()
+    {
+        EmitSignal(nameof(End), Subject);
+        QueueFree();
+    }
 }
