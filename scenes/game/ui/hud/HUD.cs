a40b094502b841e02be229e593db55e215b1d68b
Alan Rostem <arostem@gmail.com>
2021-02-21 21:29:35 +0100

    it works but i dont like it

--- scenes/game/ui/hud/HUD.cs
+++ scenes/game/ui/hud/HUD.cs
@@ -6,10 +6,16 @@ public class HUD : Control
 	[Signal]
 	public delegate void HealthChanged(uint hp);
 
+    public override void _Ready()
+    {
+        var player = GetNode<Player>("../../Level1/Player");
+        player.Connect(nameof(Player.UpdateHealth), this, nameof(UpdateHealth));
+    }
+
 	public void UpdateHealth(uint hp)
 	{
 		GD.Print("Health: ", hp);
-	}
+    }
 
 	public void UpdateScrapCount(uint hp)
 	{
