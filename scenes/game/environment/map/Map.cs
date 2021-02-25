d2a90bed2fccba45f1c17897ab2498d6a8ac0f76
Alan Rostem <arostem@gmail.com>
2021-02-25 23:21:16 +0100

    game pausing works now!

--- scenes/game/environment/map/Map.cs
+++ scenes/game/environment/map/Map.cs
@@ -4,4 +4,19 @@ using System;
 public class Map : Node2D
 {
 	public Player PlayerNode;
+	private World _world;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		_world = GetNode<World>("World");
+	}
+
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionJustPressed("pause"))
+		{
+			GetTree().Paused = !GetTree().Paused;
+		}
+	}
 }
