526bd80b3e9858875c340c5ce82be9776bf5d453
Alan Rostem <arostem@gmail.com>
2021-02-26 19:00:29 +0100

    messing with UI stuff

--- scenes/game/ui/hud/HUD.cs
+++ scenes/game/ui/hud/HUD.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class HUD : Control
+public class HUD : Container
 {
 	
 }
--- scenes/game/ui/inventory/InventoryUI.cs
+++ scenes/game/ui/inventory/InventoryUI.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class InventoryUI : Control
+public class InventoryUI : TabContainer
 {
 	public override void _Ready()
 	{
@@ -11,12 +11,5 @@ public class InventoryUI : Control
 	private void _OnOpen()
 	{
 		Visible = !Visible;
-	}
-
-	public override void _Draw()
-	{
-		var rect = new Rect2(new Vector2(), RectSize);
-		var color = new Color(255, 255, 0);
-		DrawRect(rect, color);
 	}
 }
