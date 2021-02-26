b89418eb7ac604828e8d08ac9ac6f18f3c745cd8
Alan Rostem <arostem@gmail.com>
2021-02-26 19:11:04 +0100

    input keys for switching tabs in inventory

--- scenes/game/ui/inventory/InventoryUI.cs
+++ scenes/game/ui/inventory/InventoryUI.cs
@@ -8,6 +8,19 @@ public class InventoryUI : TabContainer
 		Visible = false;
 	}
 
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionJustPressed("ui_inventory_switch_tab_left"))
+		{
+			CurrentTab = (CurrentTab - 1) % GetTabCount();
+		}
+
+		if (Input.IsActionJustPressed("ui_inventory_switch_tab_right"))
+		{
+			CurrentTab = (CurrentTab + 1) % GetTabCount();
+		}
+	}
+
 	private void _OnOpen()
 	{
 		Visible = !Visible;
