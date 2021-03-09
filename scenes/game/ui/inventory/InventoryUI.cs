50a470175826a4ebd59016498f0e2828e9ba401c
Alan Rostem <arostem@gmail.com>
2021-03-09 22:04:02 +0100

    Made inventory tabs separate nodes

--- scenes/game/ui/inventory/InventoryUI.cs
+++ scenes/game/ui/inventory/InventoryUI.cs
@@ -1,27 +1,11 @@
 using Godot;
 using System;
 
-public class InventoryUI : TabContainer
+public class InventoryUI : Control
 {
 	public override void _Ready()
 	{
 		Visible = false;
-	}
-
-	public override void _Process(float delta)
-	{
-		if (Input.IsActionJustPressed("ui_inventory_switch_tab_left"))
-		{
-			if (CurrentTab == 0)
-				CurrentTab = GetTabCount()-1;
-			else
-				CurrentTab--;
-		}
-
-		if (Input.IsActionJustPressed("ui_inventory_switch_tab_right"))
-		{
-			CurrentTab = (CurrentTab + 1) % GetTabCount();
-		}
 	}
 
 	private void _OnOpen()
