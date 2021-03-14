91d8ac24ea4a1bd4c37079032450e8ed1cee22aa
Alan Rostem <arostem@gmail.com>
2021-03-14 23:57:57 +0100

    Update ResourceAbility.cs

--- scenes/game/weapon/ability/resource_abiity/ResourceAbility.cs
+++ scenes/game/weapon/ability/resource_abiity/ResourceAbility.cs
@@ -3,79 +3,80 @@ using System;
 
 public class ResourceAbility : WeaponAbility
 {
-	[Export] 
-	public uint DrainPerTick = 1;
-	
-	[Export]
-	public float LingerDuration = 0.6f;
-	
-	[Export]
-	public float DrainInterval = 0.1f; 
-	
-	private float _currentDrainTime = 0;
-	private bool _isActive = false;
-	
-	[Signal]
-	public delegate void Linger();
-	
-	public override void _Process(float delta)
-	{
-		var type = (int)FuelType;
-		var player = GetWeapon().OwnerPlayer;
-		var fuels = player.PlayerInventory.OrdinanceFuels;
-		
-		if (fuels[type] < DrainPerTick) return;
+    [Export] public uint DrainPerTick = 1;
 
-			if (Input.IsActionPressed("tactical_ability"))
-		{
-			EmitSignal(nameof(Linger));
-			// TODO: Drain the resource
-			if (!_isActive)
-			{
-				_isActive = true;
-				OnActivate();
-			}
-		}
+    [Export] public float LingerDuration = 0.6f;
 
-		if (!_isActive) return;
-		
-		OnUpdate();
+    [Export] public float DrainInterval = 0.1f;
 
-		if (_currentDrainTime >= DrainInterval)
-		{
-			_currentDrainTime = 0;
-			fuels[type] -= DrainPerTick;
-			player.KnowInventoryOrdinanceFuelCount(fuels[type], FuelType);
-			OnTick();
-		}
-		
-		_currentDrainTime += delta;
-	}
-	
-	public virtual void OnActivate()
-	{
-		
-	}
+    private float _currentDrainTime = 0;
+    private bool _isActive = false;
 
-	public virtual void OnTick()
-	{
+    [Signal]
+    public delegate void Linger();
 
-	}
+    public override void _Process(float delta)
+    {
+        var type = (int) FuelType;
+        var player = GetWeapon().OwnerPlayer;
+        var fuels = player.PlayerInventory.OrdinanceFuels;
 
-	public virtual void OnUpdate()
-	{
+        if (fuels[type] < DrainPerTick)
+        {
+            _LingerStopped();
+            return;
+        }
 
-	}
+        if (Input.IsActionPressed("tactical_ability"))
+        {
+            EmitSignal(nameof(Linger));
+            // TODO: Drain the resource
+            if (!_isActive)
+            {
+                _isActive = true;
+                OnActivate();
+            }
+        }
 
-	public virtual void OnDeActivate()
-	{
-		
-	}
-	
-	private void _LingerStopped()
-	{
-		_currentDrainTime = 0;
-		_isActive = false;
-		OnDeActivate();
-	}
+        if (!_isActive)
+        {
+            _LingerStopped();
+            return;
+        }
+
+        OnUpdate();
+
+        if (_currentDrainTime >= DrainInterval)
+        {
+            _currentDrainTime = 0;
+            fuels[type] -= DrainPerTick;
+            player.KnowInventoryOrdinanceFuelCount(fuels[type], FuelType);
+            OnTick();
+        }
+
+        _currentDrainTime += delta;
+    }
+
+    public virtual void OnActivate()
+    {
+    }
+
+    public virtual void OnTick()
+    {
+    }
+
+    public virtual void OnUpdate()
+    {
+    }
+
+    public virtual void OnDeActivate()
+    {
+    }
+
+    private void _LingerStopped()
+    {
+        _currentDrainTime = 0;
+        _isActive = false;
+        OnDeActivate();
+    }
 }
