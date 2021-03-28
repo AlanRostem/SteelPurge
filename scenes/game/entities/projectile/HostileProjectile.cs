9dd8232037303bfe1ccbd46efae3e933965552f9
Alan Rostem <arostem@gmail.com>
2021-03-28 18:18:52 +0200

    Started on hostile projectile and fixed bug in regular projectile

--- scenes/game/entities/projectile/HostileProjectile.cs
+++ scenes/game/entities/projectile/HostileProjectile.cs
@@ -1,23 +1,22 @@
 using Godot;
 using System;
 
-public class Projectile : KinematicBody2D
+public class HostileProjectile : KinematicBody2D
 {
 	[Export] public float DirectionAngle = 0;
+	[Export] public int DamageDirection = 0;
 	[Export] public float MaxVelocity = 250;
+	[Export] public uint Damage = 15;
 	private bool _hasDisappeared = false;
 	
-	public Weapon OwnerWeapon { get; private set; }
-
 	public Vector2 Velocity;
 
-	public void Init(Weapon owner)
+	public void Init()
 	{
 		var angle = Mathf.Deg2Rad(DirectionAngle);
 		Velocity = new Vector2(
 			MaxVelocity * Mathf.Cos(angle),
 			MaxVelocity * Mathf.Sin(angle));
-		OwnerWeapon = owner;
 	}
 
 	public override void _PhysicsProcess(float delta)
@@ -25,11 +24,10 @@ public class Projectile : KinematicBody2D
 		MoveAndCollide(Velocity * delta);
 	}
 	
-	private void _OnVulnerableHitBoxEntered(object area)
+	private void _OnPlayerHit(object body)
 	{
-		var hitBox = (VulnerableHitbox) area;
-		hitBox.EmitSignal(nameof(VulnerableHitbox.Hit), OwnerWeapon.DamagePerShot);
-		OwnerWeapon.EmitSignal(nameof(Weapon.DamageDealt), OwnerWeapon.DamagePerShot, hitBox);
+		var player = (Player) body;
+		player.TakeDamage(Damage, DamageDirection);
 		if (!_hasDisappeared)
 		{
 			_hasDisappeared = true;
