30ab96150575de08a4ef2544009a306c5094e8ae
Alan Rostem <arostem@gmail.com>
2021-04-02 22:37:11 +0200

    Re-nerfed it

--- scenes/game/entities/player/Player.cs
+++ scenes/game/entities/player/Player.cs
@@ -12,7 +12,7 @@ public class Player : Entity
 	
 	private static readonly float MaxJumpHeight = CustomTileMap.Size * 6;
 	private static readonly float MinJumpHeight = CustomTileMap.Size * 1;
-	private static readonly float JumpHeightReduction = CustomTileMap.Size;
+	private static readonly float JumpHeightReduction = CustomTileMap.Size * 2;
 	private static readonly float JumpHeightRegeneration = CustomTileMap.Size * 10;
 	private static readonly float JumpDuration = .5f;
 	
