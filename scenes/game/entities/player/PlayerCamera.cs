8ba4619f41391f663bed8c53a36096030ca8744e
Alan Rostem <arostem@gmail.com>
2021-04-02 19:44:28 +0200

    I like the camera now

--- scenes/game/entities/player/PlayerCamera.cs
+++ scenes/game/entities/player/PlayerCamera.cs
@@ -3,18 +3,33 @@ using System;
 
 public class PlayerCamera : Camera2D
 {
-	/*
-	
+	public float MaxMargin = 0.4f;
+	public float MinMargin = 0.08f;
+	public float Smoothness = 0.03f;
 	private Player _player;
+
 	public override void _Ready()
 	{
 		_player = GetParent<Player>();
 	}
-	
+
 	public override void _Process(float delta)
 	{
-		base._Process(delta);
+		if (_player.EquippedWeapon.IsFiring)
+		{
+			if (_player.HorizontalLookingDirection < 0)
+			{
+				DragMarginRight = Mathf.Lerp(DragMarginRight, MaxMargin, Smoothness);
+			}
+			else if (_player.HorizontalLookingDirection > 0)
+			{
+				DragMarginLeft = Mathf.Lerp(DragMarginLeft, MaxMargin, Smoothness);
+			}
+		}
+		else
+		{
+			DragMarginLeft = Mathf.Lerp(DragMarginLeft, MinMargin, Smoothness);
+			DragMarginRight = Mathf.Lerp(DragMarginRight, MinMargin, Smoothness);
+		}
 	}
-	
-	*/
 }
