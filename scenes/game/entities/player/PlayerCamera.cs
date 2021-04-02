74dfde17f6e2f3cb8d642de9429b3fb7c36e12fb
Alan Rostem <arostem@gmail.com>
2021-04-02 18:45:41 +0200

    trying some more camera stuff

---
+++ scenes/game/entities/player/PlayerCamera.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class PlayerCamera : Camera2D
+{
+	/*
+	
+	private Player _player;
+	public override void _Ready()
+	{
+		_player = GetParent<Player>();
+	}
+	
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+	}
+	
+	*/
+}
