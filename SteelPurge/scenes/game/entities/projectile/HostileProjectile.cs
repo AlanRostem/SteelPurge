5456a5988b4be03e94ce0107bb81871b6c0240a6
Alan Rostem <arostem@gmail.com>
2021-05-15 12:36:32 +0200

    changed control but slide melee needs to ram

--- SteelPurge/scenes/game/entities/projectile/HostileProjectile.cs
+++ SteelPurge/scenes/game/entities/projectile/HostileProjectile.cs
@@ -3,51 +3,51 @@ using System;
 
 public class HostileProjectile : KinematicEntity
 {
-    [Export] public float DirectionAngle = 0;
-    [Export] public int DamageDirection = 0;
-    [Export] public float MaxVelocity = 250;
-    [Export] public uint Damage = 15;
-    private bool _hasDisappeared = false;
+	[Export] public float DirectionAngle = 0;
+	[Export] public int DamageDirection = 0;
+	[Export] public float MaxVelocity = 250;
+	[Export] public uint Damage = 15;
+	private bool _hasDisappeared = false;
 
 
-    public void InitWithAngularVelocity()
-    {
-        var angle = Mathf.Deg2Rad(DirectionAngle);
-        Velocity = new Vector2(
-            MaxVelocity * Mathf.Cos(angle),
-            MaxVelocity * Mathf.Sin(angle));
-    }
+	public void InitWithAngularVelocity()
+	{
+		var angle = Mathf.Deg2Rad(DirectionAngle);
+		Velocity = new Vector2(
+			MaxVelocity * Mathf.Cos(angle),
+			MaxVelocity * Mathf.Sin(angle));
+	}
 
-    public void InitWithHorizontalVelocity()
-    {
-        Velocity = new Vector2(DamageDirection * MaxVelocity, 0);
-    }
-
-
-    public override void _PhysicsProcess(float delta)
-    {
-        MoveAndCollide(Velocity * delta);
-    }
+	public void InitWithHorizontalVelocity()
+	{
+		Velocity = new Vector2(DamageDirection * MaxVelocity, 0);
+	}
 
 
-    private void _OnBodyHit(object body)
-    {
-        if (body is Player player)
-        {
-            player.TakeDamage(Damage, new Vector2(DamageDirection, 0));
-        }
-
-        if (!_hasDisappeared)
-        {
-            _hasDisappeared = true;
-            _OnDisappear();
-            QueueFree();
-        }
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		MoveAndCollide(Velocity * delta);
+	}
 
 
-    public virtual void _OnDisappear()
-    {
-        QueueFree();
-    }
+	private void _OnBodyHit(object body)
+	{
+		if (body is Player player)
+		{
+			player.TakeDamage(Damage, new Vector2(DamageDirection, 0));
+		}
+
+		if (!_hasDisappeared)
+		{
+			_hasDisappeared = true;
+			_OnDisappear();
+			QueueFree();
+		}
+	}
+
+
+	public virtual void _OnDisappear()
+	{
+		QueueFree();
+	}
 }
