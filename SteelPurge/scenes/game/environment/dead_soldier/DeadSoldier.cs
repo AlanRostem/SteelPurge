db637399db5f8d820b91fe1c8c8402ed4866a714
Alan Rostem <arostem@gmail.com>
2021-05-22 13:31:51 +0200

    nicely done

--- SteelPurge/scenes/game/environment/dead_soldier/DeadSoldier.cs
+++ SteelPurge/scenes/game/environment/dead_soldier/DeadSoldier.cs
@@ -3,12 +3,29 @@ using System;
 
 public class DeadSoldier : Node2D
 {
-	[Export] public Inventory.InventoryWeapon Weapon = Inventory.InventoryWeapon.Falcon;
+	private static readonly Texture[] _weaponHeldTextures = new[]
+	{
+		null, // Falcon
+		GD.Load<Texture>("res://assets/texture/dead_soldier_with_firewall.png"), // Firewall
+		GD.Load<Texture>("res://assets/texture/dead_soldier_with_joule.png"), // Joule
+		GD.Load<Texture>("res://assets/texture/dead_soldier.png"), // Count (Basically none)
+	};
 	
+	[Export] public Inventory.InventoryWeapon Weapon = Inventory.InventoryWeapon.Falcon;
+
+	private Sprite _sprite;
+
+	public override void _Ready()
+	{
+		_sprite = GetNode<Sprite>("Sprite");
+		_sprite.Texture = _weaponHeldTextures[(int) Weapon];
+	}
+
 	private void _OnInteract(Player player)
 	{
 		if (player.PlayerInventory.HasWeapon(Weapon)) return;
 		player.PlayerInventory.AddWeapon(Weapon);
 		player.PlayerInventory.SwitchWeapon(Weapon);
+		_sprite.Texture = _weaponHeldTextures[(int) Inventory.InventoryWeapon.Count];
 	}
 }
