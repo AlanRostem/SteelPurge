7d48ad504a2e75cf06d0126fd31e75750f7e71ed
Alan Rostem <arostem@gmail.com>
2021-05-23 12:08:04 +0200

    i think this one's done

--- SteelPurge/scenes/game/entities/bosses/death_hornet/HornetRogue.cs
+++ SteelPurge/scenes/game/entities/bosses/death_hornet/HornetRogue.cs
@@ -3,11 +3,14 @@ using System;
 
 public class HornetRogue : Enemy
 {
+	[Export] public uint DamagePerHit = 45;
+	[Export] public int Direction = -1;
 	private AnimatedSprite _sprite;
 	public override void _Ready()
 	{
 		base._Ready();
 		_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+		Velocity.x = Direction * 110;
 	}
 
 	public override void _Process(float delta)
@@ -16,12 +19,24 @@ public class HornetRogue : Enemy
 		_sprite.FlipH = Velocity.x < 0;
 	}
 
+	public override void _OnCollision(KinematicCollision2D collider)
+	{
+		if (IsOnWall())
+			QueueFree();
+	}
+
 	public override void TakeDamage(uint damage, Vector2 direction)
 	{
 		base.TakeDamage(damage, direction);
-		if (direction.x != 0)
+		if (direction.y != 0)
 		{
 			IsGravityEnabled = false;
 		}
+	}
+	
+	private void _OnPlayerEnterExplosiveArea(Player player)
+	{
+		player.TakeDamage(DamagePerHit, new Vector2(Direction, 0));
+		QueueFree();
 	}
 }
