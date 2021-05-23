6749d6df18062e4c5770bf3ac90d826a1382d6a6
Alan Rostem <arostem@gmail.com>
2021-05-23 12:14:10 +0200

    made a test area

--- SteelPurge/scenes/game/entities/bosses/death_hornet/HornetRogue.cs
+++ SteelPurge/scenes/game/entities/bosses/death_hornet/HornetRogue.cs
@@ -6,11 +6,11 @@ public class HornetRogue : Enemy
 	[Export] public uint DamagePerHit = 45;
 	[Export] public int Direction = -1;
 	private AnimatedSprite _sprite;
+
 	public override void _Ready()
 	{
 		base._Ready();
 		_sprite = GetNode<AnimatedSprite>("AnimatedSprite");
-		Velocity.x = Direction * 110;
 	}
 
 	public override void _Process(float delta)
@@ -19,9 +19,16 @@ public class HornetRogue : Enemy
 		_sprite.FlipH = Velocity.x < 0;
 	}
 
+	protected override void _OnMovement(float delta)
+	{
+		base._OnMovement(delta);
+		if (IsOnFloor())
+			Velocity.x = Direction * 110;
+	}
+
 	public override void _OnCollision(KinematicCollision2D collider)
 	{
-		if (IsOnWall())
+		if (IsOnWall() || IsOnCeiling())
 			QueueFree();
 	}
 
@@ -33,7 +40,7 @@ public class HornetRogue : Enemy
 			IsGravityEnabled = false;
 		}
 	}
-	
+
 	private void _OnPlayerEnterExplosiveArea(Player player)
 	{
 		player.TakeDamage(DamagePerHit, new Vector2(Direction, 0));
