ded9d6fd35418ddc38969411bffe049cd90768e8
Alan Rostem <arostem@gmail.com>
2021-06-14 20:27:59 +0200

    removed the cool idea and buffed it, have a bug with dash tho

--- SteelPurge/scenes/game/weapon/weapons/falcon/TalconFiringDevice.cs
+++ SteelPurge/scenes/game/weapon/weapons/falcon/TalconFiringDevice.cs
@@ -6,23 +6,11 @@ using System.Collections.Generic;
 public class TalconFiringDevice : FiringDevice
 {
 	private static readonly PackedScene
-		WindSliceScene = GD.Load<PackedScene>("res://scenes/game/weapon/weapons/falcon/WindSlice.tscn");
-	private static readonly PackedScene
 		AerialSliceScene = GD.Load<PackedScene>("res://scenes/game/weapon/weapons/falcon/AerialSlice.tscn");
 	
 	public override void OnFire()
 	{
-		var player = GetWeapon().OwnerPlayer;
-		if (player.IsOnFloor() && !player.IsAimingDown && !player.IsAimingUp)
-		{
-			var slice = FireProjectile(WindSliceScene);
-			slice.Position = GetWeapon().OwnerPlayer.Position;
-			slice.Scale = new Vector2(GetWeapon().OwnerPlayer.HorizontalLookingDirection, 1);
-		}
-		else
-		{
-			var slice = FireProjectile(AerialSliceScene);
-			slice.Scale = new Vector2(GetWeapon().OwnerPlayer.HorizontalLookingDirection, 1);
-		}
+		var slice = FireProjectile(AerialSliceScene);
+		slice.Scale = new Vector2(GetWeapon().OwnerPlayer.HorizontalLookingDirection, 1);
 	}
 }
