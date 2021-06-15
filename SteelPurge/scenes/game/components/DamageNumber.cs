b964398d71dfebff4046aa4288f486547492703b
Alan Rostem <arostem@gmail.com>
2021-06-15 10:11:22 +0200

    Should work with damage numbers

--- SteelPurge/scenes/game/components/DamageNumber.cs
+++ SteelPurge/scenes/game/components/DamageNumber.cs
@@ -3,18 +3,26 @@ using System;
 
 public class DamageNumber : Label
 {
+	[Signal]
+	public delegate void Disappear();
+	
 	[Export] public float RiseSpeed = 10;
 
 	private Timer _existenceTimer;
 	private float _distanceRisen = 0;
+	private uint _damage = 0;
 
 	public uint Damage
 	{
+		get => _damage;
 		set
 		{
 			Text = value.ToString();
-			_existenceTimer.Start();
-			RectPosition = new Vector2(RectPosition.x, RectPosition.y + _distanceRisen);
+			if (_existenceTimer is null)
+				_existenceTimer = GetNode<Timer>("ExistenceTimer");
+			_existenceTimer.CallDeferred("start");
+			RectPosition = new Vector2(RectPosition.x, RectPosition.y - _distanceRisen);
+			_damage = value;
 			_distanceRisen = 0;
 		}
 	}
@@ -32,7 +40,8 @@ public class DamageNumber : Label
 	
 	private void _OnExistenceTimeout()
 	{
-		QueueFree();
+		EmitSignal(nameof(Disappear));
+		CallDeferred("queue_free");
 	}
 }
 
--- SteelPurge/scenes/game/components/VulnerableHitbox.cs
+++ SteelPurge/scenes/game/components/VulnerableHitbox.cs
@@ -7,11 +7,40 @@ using Godot;
 public class VulnerableHitbox : Area2D
 {
 
+	private static readonly PackedScene DamageNumberScene =
+		GD.Load<PackedScene>("res://scenes/game/components/DamageNumber.tscn");
+
+	private static readonly Vector2 StartOffset = new Vector2(-16, -32);
+	
+	private DamageNumber _damageNumber;
+	
 	[Signal]
 	public delegate void Hit(uint damage, Vector2 knockBackDirection);
 
 	public void TakeHit(uint damage, Vector2 knockBackDirection)
 	{
 		EmitSignal(nameof(Hit), damage, knockBackDirection);
+		if (GetParent() is KinematicEntity entity) // TODO: Consider further cases
+		{
+			if (_damageNumber is null)
+			{
+				var number = (DamageNumber) DamageNumberScene.Instance();
+				number.Damage = damage;
+				number.RectPosition = entity.Position + StartOffset;
+				entity.ParentWorld.AddChild(number);
+				_damageNumber = number;
+				number.Connect(nameof(DamageNumber.Disappear), this, nameof(_OnDamageNumberDisappear));
+			}
+			else
+			{
+				_damageNumber.RectPosition = entity.Position + StartOffset;
+				_damageNumber.Damage += damage;
+			}
+		}
+	}
+
+	private void _OnDamageNumberDisappear()
+	{
+		_damageNumber = null;
 	}
 }
