7875df1b0a24f67fd72d92c3a22146ace1fbf0fc
Alan Rostem <arostem@gmail.com>
2021-06-15 18:22:05 +0200

    Healing using scrap

--- SteelPurge/scenes/game/entities/collectible/scrap/Scrap.cs
+++ SteelPurge/scenes/game/entities/collectible/scrap/Scrap.cs
@@ -6,6 +6,19 @@ public class Scrap : FallingCollectible
 	[Export] public uint Count = 10;
 	public override void OnCollected(Player player)
 	{
-		player.PlayerInventory.PickUpScrap(Count);
+		if (player.Health < player.MaxHealth)
+		{
+			var heal = Count / 2;
+			if (heal == 0)
+				heal = 1;
+			player.Health += heal;
+			if (player.Health != player.MaxHealth) return;
+			var diff = player.MaxHealth - player.Health;
+			player.PlayerInventory.PickUpScrap(diff);
+		}
+		else
+		{
+			player.PlayerInventory.PickUpScrap(Count);
+		}
 	}
 }
