197fd1bae11d2c074d23b02b569a9fb0338faa50
Alan Rostem <arostem@gmail.com>
2021-06-15 16:27:37 +0200

    Damage numbers are red when killing enemies

--- SteelPurge/scenes/game/components/damage_number/DamageNumberGenerator.cs
+++ SteelPurge/scenes/game/components/damage_number/DamageNumberGenerator.cs
@@ -5,24 +5,33 @@ public class DamageNumberGenerator : Node2D
 {
 	private static readonly PackedScene DamageNumberScene =
 		GD.Load<PackedScene>("res://scenes/game/components/damage_number/DamageNumber.tscn");
-	
+
 	private static readonly Vector2 StartOffset = new Vector2(-16, -32);
-	
+
 	private DamageNumber _damageNumber;
 
+
 	public void ShowDamageNumber(uint damage, Vector2 position, World parentWorld)
+	{
+		ShowDamageNumber(damage, position, parentWorld, Colors.White);
+	}
+
+	public void ShowDamageNumber(uint damage, Vector2 position, World parentWorld, Color color)
 	{
 		if (_damageNumber is null)
 		{
 			var number = (DamageNumber) DamageNumberScene.Instance();
 			number.Damage = damage;
 			number.RectPosition = position;
+			number.Modulate = color;
 			parentWorld.AddChild(number);
 			_damageNumber = number;
 			number.Connect(nameof(DamageNumber.Disappear), this, nameof(_OnDamageNumberDisappear));
 		}
 		else
 		{
+			if (_damageNumber.Modulate != color)
+				_damageNumber.Modulate = color;
 			_damageNumber.RectPosition = position;
 			_damageNumber.Damage += damage;
 		}
--- SteelPurge/scenes/game/entities/Enemy.cs
+++ SteelPurge/scenes/game/entities/Enemy.cs
@@ -5,141 +5,145 @@ using Object = Godot.Object;
 
 public class Enemy : KinematicEntity
 {
-    private static readonly Vector2 DamageNumberOffsetPos = new Vector2(-12, -20);
+	private static readonly Vector2 DamageNumberOffsetPos = new Vector2(-12, -20);
 
-    protected static readonly PackedScene ScrapScene =
-        GD.Load<PackedScene>("res://scenes/game/entities/collectible/scrap/Scrap.tscn");
+	protected static readonly PackedScene ScrapScene =
+		GD.Load<PackedScene>("res://scenes/game/entities/collectible/scrap/Scrap.tscn");
 
-    [Export] public uint ScrapDropHit = 2;
-    [Export] public uint ScrapDropKilled = 25;
+	[Export] public uint ScrapDropHit = 2;
+	[Export] public uint ScrapDropKilled = 25;
 
-    [Export] public uint BaseHitPoints = 45;
-    [Export] public float PlayerDetectionRange = 1000;
-    [Export] public float KnockBackSpeed = 300;
-    [Export] public bool CanBeKnockedBack = true;
+	[Export] public uint BaseHitPoints = 45;
+	[Export] public float PlayerDetectionRange = 1000;
+	[Export] public float KnockBackSpeed = 300;
+	[Export] public bool CanBeKnockedBack = true;
 
 
-    private bool _isDead;
-    private bool _dropScrap;
-    private bool _isKnockedBack;
-    public Player DetectedPlayer { get; private set; }
-    private Timer _meleeAffectedKnockBackTimer;
-    private Timer _damageIndicationTimer;
-    private DamageNumberGenerator _damageNumberGenerator;
+	private bool _isDead;
+	private bool _dropScrap;
+	private bool _isKnockedBack;
+	public Player DetectedPlayer { get; private set; }
+	private Timer _meleeAffectedKnockBackTimer;
+	private Timer _damageIndicationTimer;
+	private DamageNumberGenerator _damageNumberGenerator;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        Health = BaseHitPoints;
-        _meleeAffectedKnockBackTimer = GetNode<Timer>("MeleeAffectedKnockBackTimer");
-        _damageIndicationTimer = GetNode<Timer>("DamageIndicationTimer");
-        _damageNumberGenerator = GetNode<DamageNumberGenerator>("DamageNumberGenerator");
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		Health = BaseHitPoints;
+		_meleeAffectedKnockBackTimer = GetNode<Timer>("MeleeAffectedKnockBackTimer");
+		_damageIndicationTimer = GetNode<Timer>("DamageIndicationTimer");
+		_damageNumberGenerator = GetNode<DamageNumberGenerator>("DamageNumberGenerator");
+	}
 
-    public virtual void OnDie()
-    {
-    }
+	public virtual void OnDie()
+	{
+	}
 
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
-        if (_isDead)
-        {
-            var scrap = ParentWorld.Entities.SpawnEntityDeferred<Scrap>(ScrapScene, Position);
-            scrap.Count = ScrapDropKilled;
-            QueueFree();
-        }
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
+		if (_isDead)
+		{
+			var scrap = ParentWorld.Entities.SpawnEntityDeferred<Scrap>(ScrapScene, Position);
+			scrap.Count = ScrapDropKilled;
+			QueueFree();
+		}
 
-        if (_dropScrap)
-        {
-            _dropScrap = false;
-            var scrap = ParentWorld.Entities.SpawnEntityDeferred<Scrap>(ScrapScene, Position);
-            scrap.Count = ScrapDropHit;
-        }
+		if (_dropScrap)
+		{
+			_dropScrap = false;
+			var scrap = ParentWorld.Entities.SpawnEntityDeferred<Scrap>(ScrapScene, Position);
+			scrap.Count = ScrapDropHit;
+		}
 
-        var distance = Mathf.Abs(ParentWorld.PlayerNode.Position.x - Position.x);
-        if (distance < PlayerDetectionRange)
-        {
-            if (DetectedPlayer is null)
-                DetectedPlayer = ParentWorld.PlayerNode;
-            _ProcessWhenPlayerDetected(ParentWorld.PlayerNode);
-        }
-        else
-        {
-            _ProcessWhenPlayerNotSeen();
-        }
-    }
+		var distance = Mathf.Abs(ParentWorld.PlayerNode.Position.x - Position.x);
+		if (distance < PlayerDetectionRange)
+		{
+			if (DetectedPlayer is null)
+				DetectedPlayer = ParentWorld.PlayerNode;
+			_ProcessWhenPlayerDetected(ParentWorld.PlayerNode);
+		}
+		else
+		{
+			_ProcessWhenPlayerNotSeen();
+		}
+	}
 
-    public override void TakeDamage(uint damage, Vector2 direction, bool isCritical = false)
-    {
-        if (damage >= Health)
-        {
-            if (!_isDead)
-            {
-                OnDie();
-                // Assuming the player gave damage to the enemy
-                ParentWorld.PlayerNode.PlayerInventory.IncrementKillCount();
-                _isDead = true;
-                _damageNumberGenerator.ShowDamageNumber(Health, Position + DamageNumberOffsetPos, ParentWorld);
-                Health = 0;
-            }
-        }
-        else
-        {
-            _dropScrap = true;
-            Health -= damage;
-            _damageNumberGenerator.ShowDamageNumber(damage, Position + DamageNumberOffsetPos, ParentWorld);
-            if (!isCritical)
-                Modulate = new Color(255, 255, 255);
-            else
-                Modulate = new Color(255, 0, 0);
-            _damageIndicationTimer.Start();
-            if (direction.x != 0 || direction.y != 0)
-            {
-                KnockBack(direction);
-            }
-        }
-    }
+	public override void TakeDamage(uint damage, Vector2 direction, bool isCritical = false)
+	{
+		if (damage >= Health)
+		{
+			if (!_isDead)
+			{
+				OnDie();
+				// Assuming the player gave damage to the enemy
+				ParentWorld.PlayerNode.PlayerInventory.IncrementKillCount();
+				_isDead = true;
+				_damageNumberGenerator.ShowDamageNumber(Health, Position + DamageNumberOffsetPos, ParentWorld, Colors.Red);
+				Health = 0;
+			}
+		}
+		else
+		{
+			_dropScrap = true;
+			Health -= damage;
+			_damageNumberGenerator.ShowDamageNumber(damage, Position + DamageNumberOffsetPos, ParentWorld);
+			Modulate = !isCritical ? new Color(255, 255, 255) : new Color(255, 0, 0);
+			_damageIndicationTimer.Start();
+			if (direction.x != 0 || direction.y != 0)
+			{
+				KnockBack(direction);
+			}
+		}
+	}
 
-    public void KnockBack(Vector2 direction, float speed)
-    {
-        if (!CanBeKnockedBack) return;
-        ApplyForce(direction * speed);
-        CanMove = false;
-        _isKnockedBack = true;
-        _meleeAffectedKnockBackTimer.Start();
-    }
+	public void KnockBack(Vector2 direction, float speed)
+	{
+		if (!CanBeKnockedBack) return;
+		ApplyForce(direction * speed);
+		CanMove = false;
+		_isKnockedBack = true;
+		_meleeAffectedKnockBackTimer.Start();
+	}
 
-    public void KnockBack(Vector2 direction)
-    {
-        KnockBack(direction, KnockBackSpeed);
-    }
+	public void KnockBack(Vector2 direction)
+	{
+		KnockBack(direction, KnockBackSpeed);
+	}
 
-    private void _OnVulnerableHitboxHit(uint damage, Vector2 knockBackDirection)
-    {
-        TakeDamage(damage, knockBackDirection);
-    }
+	private void _OnVulnerableHitboxHit(uint damage, Vector2 knockBackDirection)
+	{
+		TakeDamage(damage, knockBackDirection);
+	}
+	
+	
+	private void _OnCriticalHitboxHit(uint damage, Vector2 knockBackDirection)
+	{
+		TakeDamage(damage, knockBackDirection, true);
+	}
 
-    protected virtual void _ProcessWhenPlayerDetected(Player player)
-    {
-    }
 
-    protected virtual void _ProcessWhenPlayerNotSeen()
-    {
-    }
+	protected virtual void _ProcessWhenPlayerDetected(Player player)
+	{
+	}
 
-    private void _OnKnockBackEnd()
-    {
-        if (_isKnockedBack)
-        {
-            CanMove = true;
-        }
+	protected virtual void _ProcessWhenPlayerNotSeen()
+	{
+	}
 
-        _isKnockedBack = false;
-    }
+	private void _OnKnockBackEnd()
+	{
+		if (_isKnockedBack)
+		{
+			CanMove = true;
+		}
 
-    private void _OnDamageIndicationTimeout()
-    {
-        Modulate = new Color(1, 1, 1);
-    }
+		_isKnockedBack = false;
+	}
+
+	private void _OnDamageIndicationTimeout()
+	{
+		Modulate = new Color(1, 1, 1);
+	}
 }
