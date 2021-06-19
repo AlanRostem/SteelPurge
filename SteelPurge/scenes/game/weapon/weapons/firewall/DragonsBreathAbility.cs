3272289ac29a2340e51163e64d5ebdc20c16eba9
Alan Rostem <arostem@gmail.com>
2021-06-19 21:35:41 +0200

    Fire effect added on firewall ablity

--- SteelPurge/scenes/game/weapon/weapons/firewall/DragonsBreathAbility.cs
+++ SteelPurge/scenes/game/weapon/weapons/firewall/DragonsBreathAbility.cs
@@ -4,89 +4,90 @@ using Godot.Collections;
 
 public class DragonsBreathAbility : ResourceAbility
 {
-    private readonly Dictionary<ulong, VulnerableHitbox> _hitBoxes = new Dictionary<ulong, VulnerableHitbox>();
+	private readonly Dictionary<ulong, VulnerableHitbox> _hitBoxes = new Dictionary<ulong, VulnerableHitbox>();
 
-    [Export] public uint DamagePerTick = 10;
+	[Export] public uint DamagePerTick = 10;
 
-    [Signal]
-    public delegate void TurnOn();
+	[Signal]
+	public delegate void TurnOn();
 
-    [Signal]
-    public delegate void TurnOff();
+	[Signal]
+	public delegate void TurnOff();
 
-    private FireArea _fireArea;
+	private FireArea _fireArea;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _fireArea = GetNode<FireArea>("FireArea");
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		_fireArea = GetNode<FireArea>("FireArea");
+	}
 
-    public override void OnActivate()
-    {
-        EmitSignal(nameof(TurnOn));
-        GetWeapon().IsFiring = true;
-        GetWeapon().EmitSignal(nameof(Weapon.CancelFire));
-    }
+	public override void OnActivate()
+	{
+		EmitSignal(nameof(TurnOn));
+		GetWeapon().IsFiring = true;
+		GetWeapon().EmitSignal(nameof(Weapon.CancelFire));
+	}
 
-    public override void OnDeActivate()
-    {
-        EmitSignal(nameof(TurnOff));
-        _hitBoxes.Clear();
-        GetWeapon().IsFiring = false;
-    }
+	public override void OnDeActivate()
+	{
+		EmitSignal(nameof(TurnOff));
+		_hitBoxes.Clear();
+		GetWeapon().IsFiring = false;
+	}
 
-    public override void _Process(float delta)
-    {
-        base._Process(delta);
+	public override void _Process(float delta)
+	{
+		base._Process(delta);
 
-        if (GetWeapon().OwnerPlayer.IsAimingDown)
-        {
-            _fireArea.RotationDegrees = 90;
-            _fireArea.Scale = new Vector2(1, 1);
-            return;
-        }
-        
-        if (GetWeapon().OwnerPlayer.IsAimingUp)
-        {
-            _fireArea.Scale = new Vector2(1, 1);
-            _fireArea.RotationDegrees = -90;
-            return;
-        }
-        
-        _fireArea.Rotation = 0;
+		if (GetWeapon().OwnerPlayer.IsAimingDown)
+		{
+			_fireArea.RotationDegrees = 90;
+			_fireArea.Scale = new Vector2(1, 1);
+			return;
+		}
+		
+		if (GetWeapon().OwnerPlayer.IsAimingUp)
+		{
+			_fireArea.Scale = new Vector2(1, 1);
+			_fireArea.RotationDegrees = -90;
+			return;
+		}
+		
+		_fireArea.Rotation = 0;
 
-        if (GetWeapon().OwnerPlayer.HorizontalLookingDirection < 0)
-        {
-            _fireArea.Scale = new Vector2(-1, 1);
-        }
-        else
-        {
-            _fireArea.Scale = new Vector2(1, 1);
-        }
-    }
+		if (GetWeapon().OwnerPlayer.HorizontalLookingDirection < 0)
+		{
+			_fireArea.Scale = new Vector2(-1, 1);
+		}
+		else
+		{
+			_fireArea.Scale = new Vector2(1, 1);
+		}
+	}
 
-    public override void OnTick()
-    {
-        var player = GetWeapon().OwnerPlayer;
-        if (player.VelocityY > 0 && player.IsAimingDown)
-            player.VelocityY *= 0.2f;
-        foreach (var pair in _hitBoxes)
-        {
-            var hitBox = pair.Value;
-            hitBox.TakeHit(DamagePerTick, Vector2.Zero);
-        }
-    }
+	public override void OnTick()
+	{
+		var player = GetWeapon().OwnerPlayer;
+		if (player.VelocityY > 0 && player.IsAimingDown)
+			player.VelocityY *= 0.2f;
+		foreach (var pair in _hitBoxes)
+		{
+			var hitBox = pair.Value;
+			hitBox.TakeHit(DamagePerTick, Vector2.Zero);
+			GetWeapon().EmitSignal(nameof(Weapon.DamageDealt), DamagePerTick, hitBox);
+		}
+	}
 
-    private void _HitBoxEnteredFire(object area)
-    {
-        var hitBox = (VulnerableHitbox) area;
-        _hitBoxes[hitBox.GetInstanceId()] = hitBox;
-    }
+	private void _HitBoxEnteredFire(object area)
+	{
+		var hitBox = (VulnerableHitbox) area;
+		_hitBoxes[hitBox.GetInstanceId()] = hitBox;
+	}
 
-    private void _HitBoxLeftFire(object area)
-    {
-        var hitBox = (VulnerableHitbox) area;
-        _hitBoxes.Remove(hitBox.GetInstanceId());
-    }
+	private void _HitBoxLeftFire(object area)
+	{
+		var hitBox = (VulnerableHitbox) area;
+		_hitBoxes.Remove(hitBox.GetInstanceId());
+	}
 }
