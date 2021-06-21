7ff84731d23ef17f689092e2c2bfbf15bc1a08ba
Alan Rostem <arostem@gmail.com>
2021-06-21 21:19:53 +0200

    Slam hit detection

--- SteelPurge/scenes/game/weapon/weapons/ke_6_swarm/KineticSlamAbility.cs
+++ SteelPurge/scenes/game/weapon/weapons/ke_6_swarm/KineticSlamAbility.cs
@@ -3,16 +3,20 @@ using System;
 
 public class KineticSlamAbility : TacticalAbility
 {
+	[Export] public uint DirectHitBaseDamage = 110;
+	
 	public float SlamSpeed = 300;
 
 	private bool _isJumping = false;
 	private bool _isSlamming = false;
 	private Timer _jumpTimer;
+	private CollisionShape2D _slamAreaShape;
 
 	public override void _Ready()
 	{
 		base._Ready();
 		_jumpTimer = GetNode<Timer>("JumpTimer");
+		_slamAreaShape = GetNode<CollisionShape2D>("SlamHitDetectionArea/CollisionShape2D");
 	}
 
 	public override void OnActivate()
@@ -38,6 +42,7 @@ public class KineticSlamAbility : TacticalAbility
 		player.VelocityY = SlamSpeed;
 		_isSlamming = true;
 		_isJumping = false;
+		_slamAreaShape.SetDeferred("disabled", false);
 	}
 
 	public override void OnUpdate()
@@ -57,6 +62,7 @@ public class KineticSlamAbility : TacticalAbility
 	{
 		var player = GetWeapon().OwnerPlayer;
 		player.CanMove = true;
+		_slamAreaShape.SetDeferred("disabled", true);
 		// TODO: Perform AOE damage and stun
 	}
 
@@ -65,4 +71,15 @@ public class KineticSlamAbility : TacticalAbility
 		Slam();
 	}
 	
+	// TODO: Remember to set the collision mask for normal hitboxes after this dev-phase
+	private void _OnSlamHitDetectionAreaHitBoxEntered(object area)
+	{
+		if (area is CriticalHitbox criticalHitbox)
+		{
+			criticalHitbox.TakeHit(DirectHitBaseDamage);
+			// return;
+		}
+
+		// var hitBox = (VulnerableHitbox) area;
+	}
 }
