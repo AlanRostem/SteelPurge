c22bac7eb1ab127f32624ec838cde664f380d5c8
Alan Rostem <arostem@gmail.com>
2021-06-25 23:39:24 +0200

    Healing using H28 abiliy

--- SteelPurge/scenes/game/weapon/weapons/h28_blaster/LaserShot.cs
+++ SteelPurge/scenes/game/weapon/weapons/h28_blaster/LaserShot.cs
@@ -4,11 +4,13 @@ using System;
 public class LaserShot : StaticEntity
 {
 	[Export] public uint Damage = 80;
+	[Export] public uint PlayerHealCount = 20;
 	
 	private void _OnVulnerableHitBoxHit(object area)
 	{
 		var hitBox = (VulnerableHitbox) area;
 		hitBox.TakeHit(Damage, Vector2.Zero);
+		ParentWorld.PlayerNode.Health += PlayerHealCount;
 		// TODO: Possible that this call is needed
 		// OwnerWeapon?.EmitSignal(nameof(Weapon.DamageDealt), Damage, hitBox);
 	}
