3595aabb798ef1991c625332244229728166a532
Alan Rostem <arostem@gmail.com>
2021-06-26 18:41:33 +0200

    buf gux

--- SteelPurge/scenes/game/entities/collectible/fuel/FuelCollectible.cs
+++ SteelPurge/scenes/game/entities/collectible/fuel/FuelCollectible.cs
@@ -4,7 +4,18 @@ using System;
 public class FuelCollectible : FallingCollectible
 {
 
+	private const uint MaxFades = 25;
+	
 	[Export] public uint Count = 20;
+
+	private Timer _fadeAlternationTimer;
+	private uint _fades = 0;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		_fadeAlternationTimer = GetNode<Timer>("FadeAlternationTimer");
+	}
 
 	public override bool CollectionCondition(Player player)
 	{
@@ -15,5 +26,18 @@ public class FuelCollectible : FallingCollectible
 	public override void OnCollected(Player player)
 	{
 		player.PlayerInventory.IncreaseOrdinanceFuel(player.PlayerInventory.EquippedWeaponEnum, Count);
+	}
+	
+	private void _OnStartFadingAway()
+	{
+		_fadeAlternationTimer.Start();
+	}
+	
+	private void _OnFadeAlternation()
+	{
+		SetDeferred("visible", !Visible);
+		_fades++;
+		if (_fades >= MaxFades)
+			QueueFree();
 	}
 }
