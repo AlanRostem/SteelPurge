5933d2757f1ad3658a0793fb05eee54bf5fe1163
Alan Rostem <arostem@gmail.com>
2021-06-26 16:34:22 +0200

    added it, but found a strange boss bug

--- SteelPurge/scenes/game/weapon/weapons/ke_6_swarm/KineticSlamAbility.cs
+++ SteelPurge/scenes/game/weapon/weapons/ke_6_swarm/KineticSlamAbility.cs
@@ -4,21 +4,24 @@ using System;
 public class KineticSlamAbility : TacticalAbility
 {
 	private static readonly PackedScene BlastScene =
-		GD.Load<PackedScene>("res://scenes/game/weapon/weapons/ke_6_swarm/SeismicBlast.tscn"); 
-	
+		GD.Load<PackedScene>("res://scenes/game/weapon/weapons/ke_6_swarm/SeismicBlast.tscn");
+
 	[Export] public uint DirectHitBaseDamage = 110;
-	
+
 	public float SlamSpeed = 300;
 
 	private bool _isJumping = false;
 	private bool _isSlamming = false;
+	private bool _isProtected = false;
 	private Timer _jumpTimer;
+	private Timer _protectionTimer;
 	private CollisionShape2D _slamAreaShape;
 
 	public override void _Ready()
 	{
 		base._Ready();
 		_jumpTimer = GetNode<Timer>("JumpTimer");
+		_protectionTimer = GetNode<Timer>("ProtectionTimer");
 		_slamAreaShape = GetNode<CollisionShape2D>("SlamHitDetectionArea/CollisionShape2D");
 	}
 
@@ -54,10 +57,12 @@ public class KineticSlamAbility : TacticalAbility
 
 		if (player.IsOnCeiling() && !player.IsOnFloor())
 			Slam();
-		
+
 		if (!_isJumping && _isSlamming && player.IsOnFloor())
 		{
-			DeActivate();
+			if (!_isProtected)
+				DeActivate();
+			SpawnSeismicBlast();
 		}
 	}
 
@@ -66,6 +71,11 @@ public class KineticSlamAbility : TacticalAbility
 		var player = GetWeapon().OwnerPlayer;
 		player.CanMove = true;
 		_slamAreaShape.SetDeferred("disabled", true);
+	}
+
+	private void SpawnSeismicBlast()
+	{
+		var player = GetWeapon().OwnerPlayer;
 		player.ParentWorld.Entities.SpawnStaticEntityDeferred<SeismicBlast>(BlastScene, player.Position);
 	}
 
@@ -73,7 +83,7 @@ public class KineticSlamAbility : TacticalAbility
 	{
 		Slam();
 	}
-	
+
 	// TODO: Remember to set the collision mask for normal hitboxes after this dev-phase
 	private void _OnSlamHitDetectionAreaHitBoxEntered(object area)
 	{
@@ -83,10 +93,38 @@ public class KineticSlamAbility : TacticalAbility
 			if (criticalHitbox.GetParent() is Enemy enemy)
 			{
 				enemy.ApplyStatusEffect(LivingEntity.StatusEffectType.Stun);
+				EnableProtection();
+				OnEnd();
 			}
+
 			// return;
 		}
 
 		// var hitBox = (VulnerableHitbox) area;
+	}
+
+
+	private void EnableProtection()
+	{
+		
+		_isProtected = true;
+		GetWeapon().OwnerPlayer.CanTakeDamage = false;
+		Update();
+		_protectionTimer.Start();
+	}
+
+	private void _OnProtectionEnd()
+	{
+		_isProtected = false;
+		DeActivate();
+		Update();
+		GetWeapon().OwnerPlayer.CanTakeDamage = true;
+	}
+
+	public override void _Draw()
+	{
+		if (!_isProtected) return;
+		var color = new Color(Colors.Cyan) {a = 0.5f};
+		DrawCircle(Vector2.Zero, 16, color);
 	}
 }
