76e39321d640913919722b973af946aa9a022e23
Alan Rostem <arostem@gmail.com>
2021-07-01 19:58:59 +0200

    level design for learnig ram slide

--- SteelPurge/scenes/game/environment/hazards/Hazard.cs
+++ SteelPurge/scenes/game/environment/hazards/Hazard.cs
@@ -4,9 +4,20 @@ using System;
 public class Hazard : StaticEntity
 {
 	[Export] public uint Damage = 50;
+	[Export] public bool InstaKill = true;
+	[Export] public bool TargetEnemies = true;
 
 	protected virtual void _OnEntityTouch(LivingEntity entity)
 	{
+		if (!TargetEnemies && entity is Enemy)
+			return;
+		
+		if (InstaKill)
+		{
+			entity.TakeDamage(entity.MaxHealth, Vector2.Zero);
+			return;
+		}
+		
 		entity.TakeDamage(Damage, new Vector2(-Mathf.Sign(entity.VelocityX), 0));
 	}
 }
