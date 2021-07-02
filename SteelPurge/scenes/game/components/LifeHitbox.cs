2c9b983265c77002cf85d89087bce6c6b434360e
Alan Rostem <arostem@gmail.com>
2021-07-02 17:30:29 +0200

    kinda ugly but needed

--- SteelPurge/scenes/game/components/LifeHitbox.cs
+++ SteelPurge/scenes/game/components/LifeHitbox.cs
@@ -2,10 +2,14 @@ using Godot;
 using System;
 
 /// <summary>
-/// Hitbox component that can be added to any node.
+/// Hitbox component that can be added to StaticEntity nodes.
 /// It has its own unique health pool. If the health
 /// of this hitbox is depleted, the parent is deleted
-/// and a signal is emitted. 
+/// and a signal is emitted.
+///
+/// This node is mostly used if a StaticEntity node really
+/// needs to take damage in some way when it does not inherit
+/// DestructibleObstacle. 
 /// </summary>
 public class LifeHitbox : VulnerableHitbox
 {
@@ -16,16 +20,31 @@ public class LifeHitbox : VulnerableHitbox
 	
 	public uint Health;
 
+	private DamageIndicator _damageIndicator;
+	private DamageNumberGenerator _damageNumberGenerator;
+	private StaticEntity _parent;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		_parent = GetParent<StaticEntity>();
+		_damageIndicator = GetNode<DamageIndicator>("DamageIndicator");
+		_damageNumberGenerator = GetNode<DamageNumberGenerator>("DamageNumberGenerator");
+	}
+	
 	public override void TakeHit(uint damage, Vector2 knockBackDirection, DamageType damageType)
 	{
+		_damageIndicator.Indicate(new Color(255, 255, 255));
 		base.TakeHit(damage, knockBackDirection, damageType);
 		if (damage >= Health)
 		{
 			EmitSignal(nameof(Death));
 			GetParent().QueueFree();
+			_damageNumberGenerator.ShowDamageNumber(Health, Position + new Vector2(0, -16), _parent.ParentWorld, Colors.Red);
 			return;
 		}
 
+		_damageNumberGenerator.ShowDamageNumber(damage, Position + new Vector2(0, -16), _parent.ParentWorld);
 		Health -= damage;
 	}
 }
