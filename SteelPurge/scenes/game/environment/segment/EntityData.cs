41665d516f51528357d68dd39d32565807990651
Alan Rostem <arostem@gmail.com>
2021-07-08 14:07:23 +0200

    Annoying indent shit i have to commit

--- SteelPurge/scenes/game/environment/segment/EntityData.cs
+++ SteelPurge/scenes/game/environment/segment/EntityData.cs
@@ -3,36 +3,36 @@ using Godot.Collections;
 
 public class EntityData
 {
-    private Dictionary<string, object> _data = new Dictionary<string, object>();
+	private Dictionary<string, object> _data = new Dictionary<string, object>();
 
-    public Vector2 WorldPosition
-    {
-        set => _data["position"] = new Dictionary()
-        {
-            ["x"] = value.x,
-            ["y"] = value.y,
-        };
-    }
+	public Vector2 WorldPosition
+	{
+		set => _data["position"] = new Dictionary()
+		{
+			["x"] = value.x,
+			["y"] = value.y,
+		};
+	}
 
-    public string ScenePath
-    {
-        set => _data["scenePath"] = value;
-    }
+	public string ScenePath
+	{
+		set => _data["scenePath"] = value;
+	}
 
-    public EntityData(KinematicEntity entity)
-    {
-        WorldPosition = entity.Position;
-        ScenePath = entity.Filename;
-    }
-    
-    public EntityData(StaticEntity entity)
-    {
-        WorldPosition = entity.Position;
-        ScenePath = entity.Filename;        
-    }
+	public EntityData(KinematicEntity entity)
+	{
+		WorldPosition = entity.Position;
+		ScenePath = entity.Filename;
+	}
 
-    public Dictionary<string, object> GetJson()
-    {
-        return _data;
-    }
+	public EntityData(StaticEntity entity)
+	{
+		WorldPosition = entity.Position;
+		ScenePath = entity.Filename;
+	}
+
+	public Dictionary<string, object> GetJson()
+	{
+		return _data;
+	}
 }
