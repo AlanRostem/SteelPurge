2717dea8721b1900b9a180db67bb2864a2aa83c9
Alan Rostem <arostem@gmail.com>
2021-07-10 23:34:44 +0200

    CustomTimer implemented

---
+++ SteelPurge/scenes/util/CustomTimer.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public class CustomTimer : Node
+{
+	[Signal]
+	public delegate void TimeOut();
+	
+	[Export] public float WaitTime = 1;
+	[Export] public bool AutoStart = false;
+	[Export] public bool OneShot = false;
+
+	public float TimeElapsed => _currentTime;
+	public float TimeLeft => WaitTime - _currentTime;
+
+	private float _currentTime = 0;
+	private bool _done = false;
+	private bool _paused = false;
+	
+	public override void _Ready()
+	{
+		if (AutoStart)
+			Start();
+	}
+
+	public override void _Process(float delta)
+	{
+		if (!_done && !_paused)
+		{
+			_currentTime += delta;
+		}
+
+		if (_currentTime >= WaitTime)
+		{
+			_done = true;
+			EmitSignal(nameof(TimeOut));
+			if (!OneShot)
+				Start();
+		}
+	}
+
+	public void Start(float at = 0)
+	{
+		_currentTime = at;
+		_done = false;
+		_paused = false;
+	}
+
+	public void Pause()
+	{
+		_paused = true;
+	}
+
+	public void Stop()
+	{
+		_done = true;
+		_currentTime = 0;
+	}
+}
