c4450237e32576910e6cac456611ffa1a1113bec
Alan Rostem <arostem@gmail.com>
2021-07-11 17:33:56 +0200

    Flares account for time travel

--- SteelPurge/scenes/game/weapon/weapons/firewall/Flare.cs
+++ SteelPurge/scenes/game/weapon/weapons/firewall/Flare.cs
@@ -1,7 +1,28 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public class Flare : Projectile
 {
+	private CustomTimer _lifeTimer;
+	
+	public override void _Init()
+	{
+		base._Init();
+		_lifeTimer = GetNode<CustomTimer>("LifeTimer");
+	}
 
+	public override Dictionary<string, object> ExportEntityData()
+	{
+		var data = new EntityData(base.ExportEntityData());
+		data.SetTimer(nameof(_lifeTimer), _lifeTimer);
+		return data.GetJson();
+	}
+
+	public override void FeedEntityData(Dictionary<string, object> data)
+	{
+		base.FeedEntityData(data);
+		var eData = new EntityData(data);
+		eData.ConfigureTimer(nameof(_lifeTimer), _lifeTimer);
+	}
 }
