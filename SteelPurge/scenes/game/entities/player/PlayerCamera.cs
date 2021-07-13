d31e9ca2f6c3decaf06372b897cf3ade2ad4b087
Alan Rostem <arostem@gmail.com>
2021-07-13 12:29:56 +0200

    Removed camera shift on fire

--- SteelPurge/scenes/game/entities/player/PlayerCamera.cs
+++ SteelPurge/scenes/game/entities/player/PlayerCamera.cs
@@ -12,24 +12,4 @@ public class PlayerCamera : Camera2D
 	{
 		_player = GetParent<Player>();
 	}
-
-	public override void _Process(float delta)
-	{
-		if (_player.PlayerInventory.EquippedWeapon.IsFiring)
-		{
-			if (_player.HorizontalLookingDirection < 0)
-			{
-				DragMarginRight = Mathf.Lerp(DragMarginRight, MaxMargin, Smoothness);
-			}
-			else if (_player.HorizontalLookingDirection > 0)
-			{
-				DragMarginLeft = Mathf.Lerp(DragMarginLeft, MaxMargin, Smoothness);
-			}
-		}
-		else
-		{
-			DragMarginLeft = Mathf.Lerp(DragMarginLeft, MinMargin, Smoothness);
-			DragMarginRight = Mathf.Lerp(DragMarginRight, MinMargin, Smoothness);
-		}
-	}
 }
