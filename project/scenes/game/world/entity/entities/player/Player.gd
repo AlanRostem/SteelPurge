4824d87b27dfbccee1ae09fe25877d9a5cc830fa
Alan Rostem <arostem@gmail.com>
2021-10-17 18:54:39 +0200

    added some funcs for player movement

--- project/scenes/game/world/entity/entities/player/Player.gd
+++ project/scenes/game/world/entity/entities/player/Player.gd
@@ -6,6 +6,18 @@ export var max_walk_speed: float
 var moving_direction = 1
 var can_swap_looking_direction = true
 
-func walk(direction: int, delta: float):
+func run(direction: int, delta: float):
 	accelerate_x(direction * walk_acceleration, max_walk_speed, delta)
 	moving_direction = direction
+
+func crouch_walk(direction: int, delta: float):
+	pass
+
+func slide(direction: int, delta: float):
+	pass
+	
+func crouch():
+	pass
+	
+func jump():
+	pass
