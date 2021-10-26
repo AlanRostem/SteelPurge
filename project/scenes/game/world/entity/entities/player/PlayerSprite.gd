9cbf9750100fdee19b12baf76aff7be2861d599e
Alan Rostem <arostem@gmail.com>
2021-10-26 23:32:03 +0200

    added some awesome animations!

--- project/scenes/game/world/entity/entities/player/PlayerSprite.gd
+++ project/scenes/game/world/entity/entities/player/PlayerSprite.gd
@@ -5,9 +5,13 @@ onready var player_state_machine = get_parent().get_node("PlayerFSM")
 
 func _physics_process(delta):
 	flip_h = player.horizontal_looking_direction < 0
+	var f = frame
 	match player_state_machine.get_current_state():
 		"PlayerIdleState": animation = "idle"
-		"PlayerRunState": animation = "idle"
-		"PlayerAirborneState": animation = "idle"
+		"PlayerWalkState": animation = "walking"
+		"PlayerRunState": 
+			animation = "running"
+			frame = f
+		"PlayerAirBorneState": animation = "jump"
 		"PlayerCrouchState": animation = "slide"
 		"PlayerSlideState": animation = "slide"
