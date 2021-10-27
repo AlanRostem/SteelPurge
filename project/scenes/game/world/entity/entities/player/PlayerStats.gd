1dfb1575cc0174f3a52ccc1dd85981af21cf56cc
Alan Rostem <arostem@gmail.com>
2021-10-27 15:51:37 +0200

    boilerplate code for player stats and weapon equipment

---
+++ project/scenes/game/world/entity/entities/player/PlayerStats.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+const MAX_HEALTH = 3
+const MAX_HEALING_SCRAP = 50
+const MAX_RUSH_ENERGY = 6
+
+export(PackedScene) var default_weapon_scene
+
+var __scrap_count = 0
+var __rush_energy_count = MAX_RUSH_ENERGY
+var __health = MAX_HEALTH
+var __healing_scrap = 0
+
+var __equipped_weapon
+
+func instance_and_equip_weapon(scene):
+	var weapon = scene.instance()
+	equip_weapon(weapon)
+
+func equip_weapon(weapon):
+	__equipped_weapon = weapon
+	add_child(__equipped_weapon)
+	
+func equip_default_weapon():
+	instance_and_equip_weapon(default_weapon_scene)
+	
+func get_equipped_weapon():
+	return __equipped_weapon
+
+func add_scrap(count):
+	__scrap_count += count
+	
+func lose_scrap(count):
+	__scrap_count = clamp(__scrap_count - count, 0, INF)
+	
+func get_scrap_count():
+	return __scrap_count
+
+func add_healing_scrap(count):
+	__healing_scrap += count
+	if __healing_scrap >= MAX_HEALING_SCRAP:
+		var diff = __healing_scrap - MAX_HEALING_SCRAP
+		__health += 1
+		add_healing_scrap(diff)
+
+func get_rush_energy():
+	return __rush_energy_count
+
+func use_rush_energy_half():
+	__rush_energy_count = clamp(__rush_energy_count - 1, 0, INF)
+
+func use_rush_energy_full():
+	__rush_energy_count = clamp(__rush_energy_count - 2, 0, INF)
