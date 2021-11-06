a71ce6c743ca3d8f19518648d9dbdd954288b0ec
Alan Rostem <arostem@gmail.com>
2021-11-06 18:15:02 +0100

    scrap dropping addded

--- project/scenes/game/world/entity/entities/enemy/Enemy.gd
+++ project/scenes/game/world/entity/entities/enemy/Enemy.gd
@@ -1,14 +1,32 @@
 extends MovingEntity
 class_name Enemy
 
+var scrap_scene = preload("res://scenes/game/world/entity/entities/item/collectible_items/Scrap.tscn")
+
 onready var __health_component = $HealthComponent
+
+export(int) var scrap_drop_count_damaged = 1
+export(int) var scrap_drop_count_eliminated = 10
+
+func drop_scrap(count):
+	randomize()
+	var drop_speed = 50
+	var scrap = parent_world.spawn_entity_deferred(scrap_scene, position)
+	scrap.amount_per_collect = count
+	scrap.set_velocity(Vector2(
+		rand_range(-drop_speed, drop_speed), -drop_speed
+	))
 
 func die():
 	# TODO: Implement further
 	queue_free()
+	drop_scrap(scrap_drop_count_eliminated)
 
 func _on_HealthComponent_health_depleted(health_left):
 	die()
 
 func _on_InHitBox_hit_received(hitbox, damage):
 	__health_component.take_damage(damage)
+
+func _on_HealthComponent_damage_taken(damage):
+	drop_scrap(scrap_drop_count_damaged)
--- project/scenes/game/world/entity/entities/item/collectible_items/Scrap.gd
+++ project/scenes/game/world/entity/entities/item/collectible_items/Scrap.gd
@@ -4,4 +4,3 @@ export(int) var amount_per_collect = 5
 
 func _player_collected(player):
 	player.stats.add_scrap(amount_per_collect)
-	print(player.stats.get_scrap_count())
