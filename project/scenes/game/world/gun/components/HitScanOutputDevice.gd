345ddb2386a15540f3e353ce02345794999436e8
Alan Rostem <arostem@gmail.com>
2021-11-07 13:52:05 +0100

    hitscan is stupid but it works

--- project/scenes/game/world/gun/components/HitScanOutputDevice.gd
+++ project/scenes/game/world/gun/components/HitScanOutputDevice.gd
@@ -2,26 +2,41 @@ extends "res://scenes/game/world/gun/components/FiringOutputDevice.gd"
 
 export(float) var scan_length = 100
 
-var __ray_cast = RayCast2D.new()
+var __hit_box_scene = preload("res://scenes/game/world/entity/components/hit_box/HitBox.tscn")
+
+var __hit_box: HitBox = __hit_box_scene.instance()
+var __hit_box_shape
+
+var __timer = Timer.new()
 
 func _ready():
-	__ray_cast.collide_with_areas = true
-	__ray_cast.set_collision_mask_bit(HitBox.HIT_BOX_IN_COLLISION_BIT, true)
-	__ray_cast.set_collision_mask_bit(GameWorld.SOLID_OBJECT_COLLISION_BIT, true)
-	__ray_cast.exclude_parent = true
-	parent_weapon.call_deferred("add_child", __ray_cast)
+	__hit_box_shape = CollisionShape2D.new()
+	__hit_box_shape.shape = RectangleShape2D.new()
+	__hit_box_shape.shape.extents = Vector2(scan_length, 1)
+	__hit_box_shape.disabled = true
+	__hit_box_shape.position.x += __hit_box_shape.shape.extents.x
+	__hit_box.call_deferred("add_child", __hit_box_shape)
+	
+	__hit_box.hit_box_action_type = HitBox.HitBoxActionType.HIT_DEALER
+	__hit_box.change_team(HitBox.PLAYER_TEAM)
+	
+	__hit_box.connect("hit_dealt", self, "_on_hit_box_hit_dealt")
+	
+	parent_weapon.call_deferred("add_child", __hit_box)
+	
+	add_child(__timer)
+	__timer.connect("timeout", self, "_on_Timer_timeout")
+
+func _on_hit_box_hit_dealt(hitbox):
+	hitbox.take_hit(__hit_box, parent_weapon.damage_per_shot)
+	
+func _on_Timer_timeout():
+	__hit_box_shape.disabled = true
 
 func _on_Gun_fired():
 	var player = parent_weapon.owner_player
 	var world = parent_weapon.owner_player.parent_world
-	__ray_cast.cast_to = player.get_looking_vector() * scan_length
-	__ray_cast.force_raycast_update()
-	if !__ray_cast.is_colliding(): return
-	var collider = __ray_cast.get_collider()
-	if collider is HitBox:
-		if collider.get_team() != player.hit_box.get_team():
-			# Using the player's hitbox since the raycast is not a hitbox.
-			# This may cause future bugs
-			collider.take_hit(player.hit_box, parent_weapon.damage_per_shot)
-	#elif collider is TileMap:
-	#	pass
+	__hit_box.rotation = player.get_looking_vector().angle()
+	__hit_box_shape.disabled = false
+	__timer.start(0.1)
+	
