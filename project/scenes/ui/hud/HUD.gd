0e12b40d6ea272b59617b23020ccea7dc9261da8
Alan Rostem <arostem@gmail.com>
2021-11-23 11:50:03 +0100

    made screen res smaller and removed some hud items

--- project/scenes/ui/hud/HUD.gd
+++ project/scenes/ui/hud/HUD.gd
@@ -1,15 +1,11 @@
 extends Control
 
 onready var __rush_energy_bar = $RushEnergyBar
-onready var __weapon_info = $WeaponInfo
 onready var __scrap_info = $ScrapInfo
 onready var __health_info = $HealthInfo
 
 func connect_to_player(player):
 	player.stats.connect("rush_energy_changed", __rush_energy_bar, "set_rush_energy")
-	
-	player.stats.connect("weapon_changed", __weapon_info, "set_weapon_info")
-	player.stats.connect("weapon_ammo_changed", __weapon_info, "set_ammo")
 	
 	player.stats.connect("scrap_changed", __scrap_info, "set_scrap_count")
 	
