eecdcbaf747cfbcfec29001519053e50cf22b7b0
Alan Rostem <arostem@gmail.com>
2021-11-23 16:15:18 +0100

    shooting fix

--- project/scenes/game/world/weapon/Blaster.gd
+++ project/scenes/game/world/weapon/Blaster.gd
@@ -1,10 +1,12 @@
 extends "res://scenes/game/world/weapon/Weapon.gd"
 
-func _on_Blaster_attacked():
-	pass # Replace with function body.
+var __projectile_scene = preload("res://scenes/game/world/entity/entities/projectile/projectiles/EnergyBlast.tscn")
 
-func fire_projectile(scene, dir_vec):
+func _on_Blaster_attacked():
+	fire_projectile(__projectile_scene)
+
+func fire_projectile(scene):
 	var player = get_owner_player()
 	var world = player.parent_world
 	var projectile = world.spawn_entity_deferred(scene, player.position)
-	projectile.init_from_player_weapon(dir_vec, self)
+	projectile.call_deferred("init_from_player_weapon", player.get_looking_vector(), self)
