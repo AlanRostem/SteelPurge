22560d32b79df0da1ce9945365bd8714876b3416
Alan Rostem <arostem@gmail.com>
2021-11-23 17:47:23 +0100

    man i just love it

--- project/scenes/game/world/weapon/Blaster.gd
+++ project/scenes/game/world/weapon/Blaster.gd
@@ -12,6 +12,14 @@ func fire_projectile(scene):
 	
 	var dir = player.get_looking_vector()
 	var offset = Vector2(6, -3)
+	
+	if dir.x != 0:
+		offset.x *= dir.x
+		
+	if dir.y != 0:
+		offset.x = 0
+		offset.y = 6
+		
 	projectile.call_deferred("init_from_player_weapon", dir, self, offset)
 
 
@@ -20,5 +28,5 @@ func _on_Blaster_downwards_attack():
 	
 	var player = get_owner_player()
 	if player.stats.get_rush_energy() > 0:
-		player.recoil_boost(20)
+		player.recoil_boost(35)
 		player.stats.use_rush_energy(1)
