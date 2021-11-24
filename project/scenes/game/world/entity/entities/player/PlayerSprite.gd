07d7b740519714170dc8a4c7ba88f82410b2ac78
Alan Rostem <arostem@gmail.com>
2021-11-24 18:53:43 +0100

    shit

--- project/scenes/game/world/entity/entities/player/PlayerSprite.gd
+++ project/scenes/game/world/entity/entities/player/PlayerSprite.gd
@@ -7,14 +7,30 @@ func _physics_process(delta):
 	flip_h = player.get_horizontal_looking_direction() < 0
 	var f = frame
 	match player_state_machine.get_current_state():
-		"PlayerIdleState": animation = "idle"
-		"PlayerWalkState": animation = "walking"
-		"PlayerRunState": 
+		"PlayerIdleState": 
+			animation = "idle"
+			if player.is_aiming_up():
+				animation = "aim_up_idle"
+		"PlayerWalkState": 
+			animation = "walking"
+			if player.is_aiming_up():
+				animation = "aim_up_walking"
+		"PlayerRunState":
+			if player.is_aiming_up():
+				animation = "aim_up_running"
 			animation = "running"
 			frame = f
 		"PlayerAirBorneState": 
 			animation = "jump"
-			if player.get_looking_vector().y > 0:
+			if player.is_aiming_up():
+				animation = "aim_up_jump"
+			elif player.get_looking_vector().y > 0:
 				animation = "aim_down"
-		"PlayerCrouchState": animation = "slide"
-		"PlayerSlideState": animation = "slide"
+		"PlayerCrouchState": 
+			animation = "slide"
+			if player.is_aiming_up():
+				animation = "aim_up_slide"
+		"PlayerSlideState": 
+			animation = "slide"
+			if player.is_aiming_up():
+				animation = "aim_up_slide"
