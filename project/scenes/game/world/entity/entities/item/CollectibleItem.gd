c3650b02d7f029f430cf8bfa6365d30883956d45
Alan Rostem <arostem@gmail.com>
2021-11-24 17:02:58 +0100

    collectible fixes

--- project/scenes/game/world/entity/entities/item/CollectibleItem.gd
+++ project/scenes/game/world/entity/entities/item/CollectibleItem.gd
@@ -1,8 +1,24 @@
 extends MovingEntity
 
+onready var __flash_timer = $FlashTimer
+onready var _life_timer = $LifeTimer
+
+var __flashing = false
+
+func _process(delta):
+	if _life_timer.time_left < _life_timer.wait_time / 2 and !__flashing:
+		__flash_timer.start()
+		__flashing = true
+
 func _player_collected(player):
 	pass
 
 func _on_PlayerDetectionArea_body_entered(body):
 	_player_collected(body)
 	queue_free()
+
+func _on_LifeTimer_timeout():
+	queue_free()
+
+func _on_FlashTimer_timeout():
+	visible = !visible
