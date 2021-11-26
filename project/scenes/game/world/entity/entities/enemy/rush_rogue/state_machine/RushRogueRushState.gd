6e03492bb77f51879c917a1de8626e1442e387c0
Alan Rostem <arostem@gmail.com>
2021-11-26 17:47:33 +0100

    mcrogeu

--- project/scenes/game/world/entity/entities/enemy/rush_rogue/state_machine/RushRogueRushState.gd
+++ project/scenes/game/world/entity/entities/enemy/rush_rogue/state_machine/RushRogueRushState.gd
@@ -5,7 +5,10 @@ onready var __rush_timer = $RushTimer
 func physics_update(delta):
 	if rush_rogue.is_charging_up():
 		rush_rogue.horizontal_looking_direction = rush_rogue.get_horizontal_player_detect_direction()
-
+	elif rush_rogue.is_on_wall():
+		rush_rogue.set_charging_up(true)
+		__rush_timer.start()
+	
 func _on_RushRogue_player_detected(player):
 	parent_state_machine.transition_to(name)
 
