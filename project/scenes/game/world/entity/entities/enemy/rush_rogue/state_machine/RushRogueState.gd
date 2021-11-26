135ffc070858c9efbb3b952ba522ec0c3df98bcf
Alan Rostem <arostem@gmail.com>
2021-11-26 16:53:04 +0100

    sad

--- project/scenes/game/world/entity/entities/enemy/rush_rogue/state_machine/RushRogueState.gd
+++ project/scenes/game/world/entity/entities/enemy/rush_rogue/state_machine/RushRogueState.gd
@@ -3,4 +3,4 @@ extends "res://scenes/game/world/entity/components/state_machine/State.gd"
 const MAX_PATROL_SPEED = 40
 const MAX_RUSH_SPEED = 110
 
-onready var rush_rogue = get_parent()
+onready var rush_rogue = get_parent().get_parent()
