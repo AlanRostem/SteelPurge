7f76ab485336b3e62a6d4f8ec73b09c5c5c9f4c8
Alan Rostem <arostem@gmail.com>
2021-11-26 12:18:56 +0100

    some fixes to ram slide

--- project/scenes/game/world/entity/entities/enemy/Enemy.gd
+++ project/scenes/game/world/entity/entities/enemy/Enemy.gd
@@ -4,9 +4,12 @@ class_name Enemy
 var scrap_scene = preload("res://scenes/game/world/entity/entities/item/collectible_items/Scrap.tscn")
 
 onready var __health_component = $HealthComponent
+onready var __damage_timer = $DamageAvailabilityTimer
 
 export(int) var scrap_drop_count_damaged = 1
 export(int) var scrap_drop_count_eliminated = 10
+
+var __can_deal_damage_to_player = true
 
 func drop_scrap(count):
 	randomize()
@@ -34,12 +37,21 @@ func _on_HealthComponent_damage_taken(damage):
 
 
 func _on_OutHitBox_hit_dealt(hitbox):
+	if !__can_deal_damage_to_player: return
 	var player = hitbox.get_parent()
 	if player is Player:
 		player.stats.take_one_damage()
 
+func set_can_deal_damage(value):
+	__can_deal_damage_to_player = value
 
 func _on_InHitBox_received_additional_message(message):
 	if message.has("ram_slide"):
 		set_velocity_x(0)
 		set_velocity_y(-Player.RAM_SLIDE_SPEED)
+		set_can_deal_damage(false)
+		__damage_timer.start()
+
+
+func _on_DamageAvailabilityTimer_timeout():
+	set_can_deal_damage(true)
