6be0b115b618d74e7a730acf083b87aca067d5e5
Alan Rostem <arostem@gmail.com>
2021-11-27 19:32:29 +0100

    dir change for sword

--- project/scenes/game/world/weapon/Sword.gd
+++ project/scenes/game/world/weapon/Sword.gd
@@ -9,12 +9,16 @@ onready var __hit_box_shape = $OutHitBox/CollisionShape2D
 func _physics_process(delta):
 	var player = get_owner_player()
 	__hit_box.scale.x = player.get_horizontal_looking_dir()
+	var dir = player.get_looking_vector()
+	if dir.y != 0:
+		__hit_box.rotation = dir.angle() * player.get_horizontal_looking_dir()
+	else:
+		__hit_box.rotation = 0
 
 func _on_OutHitBox_hit_dealt(hitbox):
 	hitbox.take_hit(__hit_box, STANDARD_DAMAGE, {
 		"knock_back": true
 	})
-
 
 func _on_Sword_attacked():
 	__hit_box_shape.disabled = false
@@ -22,7 +26,6 @@ func _on_Sword_attacked():
 	if player.is_on_ground():
 		player.set_velocity_x(0)
 	player.set_can_move_on_ground(false)
-
 
 func _on_Sword_attack_cycle_end():
 	var player = get_owner_player()
