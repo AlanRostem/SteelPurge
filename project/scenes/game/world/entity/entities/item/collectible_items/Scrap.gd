38a491285c861b8bf9086999af41c6100a47c6e1
Alan Rostem <arostem@gmail.com>
2021-12-01 19:17:42 +0100

    Update Scrap.gd

--- project/scenes/game/world/entity/entities/item/collectible_items/Scrap.gd
+++ project/scenes/game/world/entity/entities/item/collectible_items/Scrap.gd
@@ -4,4 +4,4 @@ export(int) var amount_per_collect = 5
 
 func _player_collected(player):
 	player.stats.add_scrap(amount_per_collect)
-	parent_world.show_hover_scrap_collected(amount_per_collect, position + Vector2.UP * 12)
+	parent_world.show_hover_scrap_collected(amount_per_collect, position + Vector2.UP * 16)
